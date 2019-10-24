30ffbfaa77e11ff3f5da7e4109f4824ad9317641
akatha647 <Akhila_Katha@comcast.com>
2019-10-24 09:49:24 +0000

    TDKB-886 : M70 Coverage Improvement
    Reason for change:Adding new scripts in EthWAN module
    Test Procedure:Run EthWAN scripts via TDK TM
    Risks: None
    Signed-off-by: Surekha <surekha.r@tataelxsi.co.in>
    
    (cherry picked from commit ac1492031245f1385c5928de8a628e42b8b95ef2)
    (cherry picked from commit a07081335ce1fdfa996dc1cc7c6e0dc49e0400ef)
    
    Change-Id: I1477b0840c80e4bf2cbeebc1c8f20775b3b05667

---
+++ testscripts/RDKB/component/sysutil/TS_SANITY_Is_DNSMASQ_UP.py
@@ -0,0 +1,167 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>2</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TS_SANITY_Is_DNSMASQ_UP</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>ExecuteCmd</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To check if dnsmasq.conf is present and dns process is running</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>1</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+    <box_type>Emulator</box_type>
+    <!--  -->
+    <box_type>RPI</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_SYSUTIL_29</test_case_id>
+    <test_objective>To check if dnsmasq.conf is present and dns proces is running</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1.Load sysutil module
+2.Retrieve the dnsmasq conf file path from properties file
+3.Check if dnsmasq conf file is present
+4.Check dnsmasq process is running
+5.Unload sysutil module</automation_approch>
+    <expected_output>dnsmasq conf file should be present and dnsmasq process should be running</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>sysutil</test_stub_interface>
+    <test_script>TS_SANITY_Is_DNSMASQ_UP</test_script>
+    <skipped>No</skipped>
+    <release_version>M70</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script ^M
+import tdklib;
+from tdkbVariables import *;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("sysutil","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_SANITY_Is_DNSMASQ_UP');
+#Get the result of connection with test component and DUT
+loadmodulestatus =obj.getLoadModuleResult();
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS");
+    expectedresult="SUCCESS";
+    tdkTestObj = obj.createTestStep('ExecuteCmd');
+    cmd = "sh %s/tdk_utility.sh parseConfigFile DNSMASQ_CONF_PATH" %TDK_PATH;
+    print cmd
+    tdkTestObj.addParameter("command", cmd);
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    dnsmasqConfPath = tdkTestObj.getResultDetails().strip().replace("\\n", "");
+    if expectedresult in actualresult and dnsmasqConfPath != "":
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "TEST STEP 1:Should get the dnsmasq conf file path";
+        print "ACTUAL RESULT 1:dnsmasq conf file path:%s" %dnsmasqConfPath;
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : SUCCESS"
+        cmd = "[ -f %s ] && echo \"File exist\" || echo \"File does not exist\"" %dnsmasqConfPath;
+        tdkTestObj.addParameter("command",cmd);
+        tdkTestObj.executeTestCase(expectedresult);
+        actualresult = tdkTestObj.getResult();
+        details = tdkTestObj.getResultDetails().strip().replace("\\n", "");
+        if details == "File exist":
+            tdkTestObj.setResultStatus("SUCCESS");
+            print "TEST STEP 2:dnsmasq conf file should be present";
+            print "ACTUAL RESULT 2:dnsmasq conf file is present";
+            #Get the result of execution
+            print "[TEST EXECUTION RESULT] : SUCCESS";
+            #check whether the process is running or not
+            query="sh %s/tdk_platform_utility.sh checkProcess dnsmasq" %TDK_PATH
+            print "query:%s" %query
+            tdkTestObj = obj.createTestStep('ExecuteCmd');
+            tdkTestObj.addParameter("command", query)
+            tdkTestObj.executeTestCase("SUCCESS");
+            actualresult = tdkTestObj.getResult();
+            pid = tdkTestObj.getResultDetails().strip().replace("\\n","");
+            if expectedresult in actualresult and pid:
+                tdkTestObj.setResultStatus("SUCCESS");
+                print "TEST STEP 3:dnsmasq process should be running";
+                print "ACTUAL RESULT 3: PID of dnsmasq %s" %pid;
+                #Get the result of execution
+                print "[TEST EXECUTION RESULT] : SUCCESS";
+            else:
+                tdkTestObj.setResultStatus("FAILURE");
+                print "TEST STEP 3:dnsmasq process should be running";
+                print "ACTUAL RESULT 3: dnsmasq is not running";
+                #Get the result of execution
+                print "[TEST EXECUTION RESULT] : FAILURE";
+        else:
+            tdkTestObj.setResultStatus("FAILURE");
+            print "TEST STEP 2:dnsmasq conf file should be present";
+            print "ACTUAL RESULT 2:dnsmasq conf file is not present";
+            #Get the result of execution
+            print "[TEST EXECUTION RESULT] : FAILURE";
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+        print "TEST STEP 1:Should get the dnsmasq conf file path";
+        print "ACTUAL RESULT 1:Failed to get dnsmasq conf file path";
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : FAILURE"
+
+    obj.unloadModule("sysutil");
+else:
+        print "Failed to load sysutil module";
+        obj.setLoadModuleStatus("FAILURE");
+        print "Module loading failed"
+
+
+
