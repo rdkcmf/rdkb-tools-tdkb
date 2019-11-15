2030e4c067e8f46b9b0bcf606782d25101379860
akatha647 <Akhila_Katha@comcast.com>
2019-11-15 11:42:05 +0000

    TDKB-917 : M71 Coverage Improvement
    Reason for change:Adding new scripts in PAM module
    Test Procedure:Run PAM scripts via TDK TM
    Risks: None
    Signed-off-by: Surekha <surekha.r@tataelxsi.co.in>
    
    (cherry picked from commit 9831e095be021d7e2d0bdf23dde706fc1613deac)
    
    Change-Id: I494dcd06d5e31aee781c390336c65c73ad929cd1

---
+++ testscripts/RDKB/component/PAM/TS_PAM_GetDefaultRFCAdvancedParentalControlStatus.py
@@ -0,0 +1,161 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>5</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TS_PAM_GetDefaultRFCAdvancedParentalControlStatus</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>pam_GetParameterValues</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To get default  advanced parental control enable status as true</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>40</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+    <box_type>RPI</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_PAM_152</test_case_id>
+    <test_objective>To get default  advanced parental control enable status as true</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband,RPI</test_setup>
+    <pre_requisite>1.Ccsp Components in DUT should be in a running state that includes component under test Cable Modem
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AdvancedParentalControl.Enable</input_parameters>
+    <automation_approch>1.Load PAM module
+2.Do factory reset
+3.Get the value of Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AdvancedParentalControl.Enable
+4.Check if default  advanced parental control enable status is true
+5.Unload the module</automation_approch>
+    <expected_output>Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AdvancedParentalControl.Enable should be true by default</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>None</test_stub_interface>
+    <test_script>TS_PAM_GetDefaultRFCAdvancedParentalControlStatus</test_script>
+    <skipped>No</skipped>
+    <release_version>M71</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+#use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("pam","RDKB");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_PAM_GetDefaultRFCAdvancedParentalControlStatus');
+
+#Get the result of connection with test component and STB
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS");
+    #save device's current state before it goes for reboot
+    obj.saveCurrentState();
+    #Initiate Factory reset before checking the default value
+    tdkTestObj = obj.createTestStep('pam_Setparams');
+    tdkTestObj.addParameter("ParamName","Device.X_CISCO_COM_DeviceControl.FactoryReset");
+    tdkTestObj.addParameter("ParamValue","Router,Wifi,VoIP,Dect,MoCA");
+    tdkTestObj.addParameter("Type","string");
+    expectedresult="SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails();
+
+    if expectedresult in actualresult:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "TEST STEP 1: Initiate factory reset ";
+        print "EXPECTED RESULT 1: Should inititate factory reset";
+        print "ACTUAL RESULT 1: %s" %details;
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : SUCCESS";
+        #Restore the device state saved before reboot
+        obj.restorePreviousStateAfterReboot();
+
+        #Script to load the configuration file of the component
+        tdkTestObj = obj.createTestStep("pam_GetParameterValues");
+        tdkTestObj.addParameter("ParamName","Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AdvancedParentalControl.Enable");
+        expectedresult="SUCCESS";
+        tdkTestObj.executeTestCase(expectedresult);
+        actualresult = tdkTestObj.getResult();
+        details = tdkTestObj.getResultDetails().strip().replace("\\n","");
+
+        if expectedresult in actualresult and "true" in details:
+            #Set the result status of execution
+            tdkTestObj.setResultStatus("SUCCESS");
+            print "TEST STEP 2:Should get the default RFC advanced parental control status as true";
+            print "ACTUAL RESULT 2:Default RFC advanced parental control status %s" %details;
+            #Get the result of execution
+            print "[TEST EXECUTION RESULT] : SUCCESS" ;
+        else:
+            tdkTestObj.setResultStatus("FAILURE");
+            print "TEST STEP 2:Should get the default RFC advanced parental control status as true";
+            print "ACTUAL RESULT 2:Default RFC advanced parental control status %s" %details;
+            #Get the result of execution
+            print "[TEST EXECUTION RESULT] : FAILURE" ;
+    else:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("FAILURE");
+        print "TEST STEP 1: Initiate factory reset ";
+        print "ACTUAL RESULT 1: %s" %details;
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : FAILURE";
+
+    obj.unloadModule("pam");
+else:
+    print "Failed to load the module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading failed";
+
+
