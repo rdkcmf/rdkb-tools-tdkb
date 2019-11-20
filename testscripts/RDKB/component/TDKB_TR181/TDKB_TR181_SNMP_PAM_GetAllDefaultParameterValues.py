b094335dbf83e36558d0aa0282c7611696614975
smrithi <smrithi_surendran@comcast.com>
2019-11-20 15:45:06 +0000

    TDKB-920 : M71 coverage improvement
    Reason for change: Add the test scripts for tr181 validation
    Test Procedure:Run scripts via TDK TM
    Risks: None
    Signed-off-by: Smrithi <smrithis@tataelxsi.co.in>
    
    Change-Id: I780a7e17237f093afda43dbfb195f861b928a41e

---
+++ testscripts/RDKB/component/TDKB_TR181/TDKB_TR181_SNMP_PAM_GetAllDefaultParameterValues.py
@@ -0,0 +1,156 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>5</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TDKB_TR181_SNMP_PAM_GetAllDefaultParameterValues</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>TDKB_TR181Stub_Get</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Check via SNMP, whether all tr181 parameters of PAM are having the default values after factory reset</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>30</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+    <box_type>Emulator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TDKB_TR181_09</test_case_id>
+    <test_objective>Check via SNMP, whether all tr181 parameters of PAM are having the default values after factory reset</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband,RPI,Emulator</test_setup>
+    <pre_requisite>TDK test agent should be running
+PAM module's parameter xml should be available</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1. Load tdkbtr181 module and sysutil module
+2. Set the validation type as SNMP
+3. Set the factoryReset flag as true
+4. Do the factory reset of device via SNMP
+5. Invoke getAllParams() by passing PAM as module name
+6. Check if all parameters are having their default values
+7. Display the final status of get and the list of failed parameters, if any
+8. Unload modules</automation_approch>
+    <expected_output>Get operation for default values on all PAM parameters via SNMP should be success</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>tdktr181</test_stub_interface>
+    <test_script>TDKB_TR181_SNMP_PAM_GetAllDefaultParameterValues</test_script>
+    <skipped>No</skipped>
+    <release_version>M71</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib;
+import tdkbSetAllParams
+from tdkbVariables import *;
+import snmplib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("tdkbtr181","1");
+obj1 = tdklib.TDKScriptingLibrary("sysutil","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TDKB_TR181_SNMP_PAM_GetAllDefaultParameterValues');
+obj1.configureTestCase(ip,port,'TDKB_TR181_SNMP_PAM_GetAllDefaultParameterValues');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus=obj.getLoadModuleResult();
+loadmodulestatus1=obj1.getLoadModuleResult();
+
+if "SUCCESS" in loadmodulestatus.upper() and "SUCCESS" in loadmodulestatus1.upper():
+    #Set the result status of execution
+    obj.setLoadModuleStatus("SUCCESS")
+    obj1.setLoadModuleStatus("SUCCESS")
+
+    tdkTestObj = obj1.createTestStep('ExecuteCmd');
+    expectedresult="SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+
+    #save device's current state before it goes for reboot
+    obj1.saveCurrentState();
+    tdkTestObj.setResultStatus("SUCCESS");
+    print "TEST STEP 2: Initiate factory reset ";
+    print "EXPECTED RESULT 2: Should initiate factory reset";
+
+    # Resetting device using snmp command
+    #Get the Community String
+    communityString = snmplib.getCommunityString(obj1,"snmpset");
+    #Get the IP Address
+    ipaddress = snmplib.getIPAddress(obj1);
+    ########## Script to Execute the snmp command ###########
+    actResponse =snmplib.SnmpExecuteCmd("snmpset", communityString, "-v 2c", "1.3.6.1.4.1.17270.50.2.1.1.1002.0 i 1", ipaddress);
+    if "INTEGER" in actResponse:
+        print "Factory reset : SUCCESS";
+        #Restore the device state saved before reboot
+        obj1.restorePreviousStateAfterReboot();
+
+        setup_type = "SNMP"
+        factoryReset = "true"
+
+        #Invoke the utility function to get and validate the values for all configured tr181 params
+        moduleStatus,failedParams = tdkbSetAllParams.getAllParams("PAM", setup_type, factoryReset, obj, obj1);
+
+        print "Status of PAM validation is ", moduleStatus, "\n";
+        if moduleStatus == "FAILURE":
+            print "The failed params are ", failedParams, "\n";
+            tdkTestObj.setResultStatus("FAILURE");
+    else:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("FAILURE");
+        print "[Factory Reset] : FAILURE";
+
+    obj.unloadModule("tdkbtr181");
+    obj1.unloadModule("sysutil");
+else:
+    print "Failed to load module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading failed";
---
+++ testscripts/RDKB/component/TDKB_TR181/TDKB_TR181_SNMP_PAM_SetAllParameterValues.py
@@ -0,0 +1,133 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>12</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TDKB_TR181_SNMP_PAM_SetAllParameterValues</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>TDKB_TR181Stub_Get</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To set all tr181 parameters in PAM module via SNMP</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>30</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+    <box_type>Emulator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TDKB_TR181_03</test_case_id>
+    <test_objective>To set all tr181 parameters in PAM module via SNMP</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband,RPI,Emulator</test_setup>
+    <pre_requisite>TDK test agent should be running
+PAM module's parameter xml should be available</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1. Load tdkbtr181 module and sysutil module
+2. Set the validation type as SNMP
+3. Invoke setAllParams() by passing PAM as module name
+4. Display the final status of set and the list of failed parameters, if any
+5. Unload modules</automation_approch>
+    <expected_output>Set operation on all PAM parameters via SNMP should be success</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>tdkbtr181</test_stub_interface>
+    <test_script>TDKB_TR181_SNMP_PAM_SetAllParameterValues</test_script>
+    <skipped>No</skipped>
+    <release_version>M71</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib;
+import tdkbSetAllParams
+from tdkbVariables import *;
+import os;
+import ConfigParser;
+
+#Test component to be tested
+obj= tdklib.TDKScriptingLibrary("tdkbtr181","1");
+obj1 = tdklib.TDKScriptingLibrary("sysutil","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TDKB_TR181_PAM_SetAllParameterValues');
+obj1.configureTestCase(ip,port,'TDKB_TR181_PAM_SetAllParameterValues');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus=obj.getLoadModuleResult();
+loadmodulestatus1=obj1.getLoadModuleResult();
+
+if "SUCCESS" in loadmodulestatus.upper() and "SUCCESS" in loadmodulestatus1.upper():
+    #Set the result status of execution
+    obj.setLoadModuleStatus("SUCCESS")
+    obj1.setLoadModuleStatus("SUCCESS")
+    tdkTestObj = obj1.createTestStep('ExecuteCmd');
+    expectedresult="SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+
+    print "The module to test is: PAM";
+
+    setup_type = "SNMP"
+    module = "PAM"
+
+    #Invoke the utility function to set and validate the values for all configured tr181 params
+    moduleStatus,failedParams= tdkbSetAllParams.setAllParams(module, setup_type, obj, obj1);
+
+    print "Status of PAM module test is : ",moduleStatus, "\n";
+    if moduleStatus == "FAILURE":
+           print "The failed params are ", failedParams, "\n";
+           tdkTestObj.setResultStatus("FAILURE");
+
+    obj1.unloadModule("sysutil");
+    obj.unloadModule("tdkbtr181");
+else:
+    print "Failed to load module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading failed";
