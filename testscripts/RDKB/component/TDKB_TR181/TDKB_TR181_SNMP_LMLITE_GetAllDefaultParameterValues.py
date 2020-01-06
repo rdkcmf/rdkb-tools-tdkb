1a01ce3722ff7f985dd1d1dd419c542bcc0d42a0
smrithi <smrithi_surendran@comcast.com>
2020-01-06 14:12:20 +0000

    TDKB-973 : Tr181 framework test coverage improvement
    Reason for change:  Marking RPI NA for SNMP scripts
    Test Procedure:Run tr181 scripts via TDK TM
    Risks: None
    Signed-off-by: Smrithi <smrithis@tataelxsi.co.in>
    
    (cherry picked from commit 2236d88ff0ada6a2bfa41f64bf726fb4ac4672df)
    
    Change-Id: I3d38d884ca5c9a6f127ecc377de198898f756d9c

--- testscripts/RDKB/component/TDKB_TR181/TDKB_TR181_SNMP_LMLITE_GetAllDefaultParameterValues.py
+++ testscripts/RDKB/component/TDKB_TR181/TDKB_TR181_SNMP_LMLITE_GetAllDefaultParameterValues.py
@@ -52,8 +52,6 @@
     <!--  -->
     <box_type>Emulator</box_type>
     <!--  -->
-    <box_type>RPI</box_type>
-    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
@@ -63,7 +61,7 @@
     <test_case_id>TC_TDKB_TR181_68</test_case_id>
     <test_objective>Check via SNMP, whether all tr181 parameters of LMLITE are having the default values after factory reset</test_objective>
     <test_type>Positive</test_type>
-    <test_setup>Broadband,RPI,Emulator</test_setup>
+    <test_setup>Broadband,Emulator</test_setup>
     <pre_requisite>TDK test agent should be running
 LMLITE module's parameter xml should be available</pre_requisite>
     <api_or_interface_used>None</api_or_interface_used>
