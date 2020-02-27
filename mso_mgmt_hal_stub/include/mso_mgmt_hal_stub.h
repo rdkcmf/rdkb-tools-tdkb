bc16de5a7c6062e41fa859fc0dab8c56c2cfb606
srajan726 <surekha_rajanbabu@comcast.com>
2020-02-27 13:31:00 +0000

    TDKB-1026: New script addition for MSOMGMTHAL
    Reason for change: adding new scripts
    Test Procedure: Run MSOMGMTHAL scripts on DUT.
    Risks: None
    Signed-off-by: Surekha <surekha.r@tataelxsi.co.in>
    
    (cherry picked from commit d604fdfd8ae2d968da55f59950277c6cbd800b8d)
    Change-Id: I7ecf272f8f81fc91199079a0d8be032b81c73d31

---
+++ mso_mgmt_hal_stub/include/mso_mgmt_hal_stub.h
@@ -0,0 +1,66 @@
+/*
+ *If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#ifndef __MSOMGMT_STUB_HAL_H__
+#define __MSOMGMT_STUB_HAL_H__
+
+#include "rdkteststubintf.h"
+#include "rdktestagentintf.h"
+#include "ssp_tdk_mso_mgmt_hal_wrp.h"
+#include <jsonrpccpp/server/connectors/tcpsocketserver.h>
+
+/* for reference added it,(IN) indicates accepting the request from Test Manager and (OUT)
+   indicates sending the response for the request back to the Manager */
+#ifndef IN
+#define IN
+#endif
+
+#ifndef OUT
+#define OUT
+#endif
+
+#define TEST_SUCCESS true
+#define TEST_FAILURE false
+
+using namespace std;
+/* RDKTestAgent : This Class provides interface for the module to enable RPC mechanism. */
+class RDKTestAgent;
+/* RDKTestStubInterface : This Class provides provides interface for the modules.  */
+class mso_mgmt_hal : public RDKTestStubInterface, public AbstractServer<mso_mgmt_hal>
+{
+        public:
+
+                mso_mgmt_hal(TcpSocketServer &ptrRpcServer) : AbstractServer <mso_mgmt_hal>(ptrRpcServer)
+                {
+                  this->bindAndAddMethod(Procedure("mso_mgmt_hal_GetMsoPodSeed", PARAMS_BY_NAME, JSON_STRING,"paramType", JSON_STRING,NULL), &mso_mgmt_hal::mso_mgmt_hal_GetMsoPodSeed);                
+                  this->bindAndAddMethod(Procedure("mso_mgmt_hal_MsoValidatePwd", PARAMS_BY_NAME,JSON_STRING,"paramValue",JSON_STRING,"paramType",JSON_STRING,NULL), &mso_mgmt_hal::mso_mgmt_hal_MsoValidatePwd);
+                }                
+                bool initialize(IN const char* szVersion);
+                bool cleanup(const char*);
+                std::string testmodulepre_requisites();
+                bool testmodulepost_requisites();
+
+                void mso_mgmt_hal_GetMsoPodSeed(IN const Json::Value& req, OUT Json::Value& response);
+                void mso_mgmt_hal_MsoValidatePwd(IN const Json::Value& req, OUT Json::Value& response);
+
+};
+#endif
+
+
+
---
+++ mso_mgmt_hal_stub/src/mso_mgmt_hal_stub.cpp
@@ -0,0 +1,190 @@
+/*
+ *If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#include "mso_mgmt_hal_stub.h"
+
+/********************************************************************************************
+ *Function name : testmodulepre_requisites
+ *Description   : testmodulepre_requisites will  be used for registering TDK with the CR
+ *@param [in]   : None
+ *@param [out]  : Return string "SUCCESS" in case of success else return string "FAILURE"
+ **********************************************************************************************/
+std::string mso_mgmt_hal::testmodulepre_requisites()
+{
+        /*Dummy function required as it is pure virtual. No need to register with CCSP bus for HAL*/
+        return "SUCCESS";
+
+}
+
+/**********************************************************************************************
+ *Function name : testmodulepost_requisites
+ *Description   : testmodulepost_requisites will be used for unregistering TDK with the CR
+ *@param [in]   : None
+ *@param [out]  : Return TEST_SUCCESS or TEST_FAILURE based on the return value
+ **********************************************************************************************/
+bool mso_mgmt_hal::testmodulepost_requisites()
+{
+        /*Dummy function required as it is pure virtual. No need to register with CCSP bus for HAL*/
+        return TEST_SUCCESS;
+}
+
+/***************************************************************************************
+ *Function name :  mso_mgmt_hal::initialize
+ *Description   : This function is used to register all the msomgmt_hal methods.
+ *param [in]    : szVersion - version, ptrAgentObj - Agent obhect
+ *@param [out]  : Return TEST_SUCCESS or TEST_FAILURE
+ ***************************************************************************************/
+bool mso_mgmt_hal::initialize(IN const char* szVersion)
+
+{
+        DEBUG_PRINT(DEBUG_TRACE, "mso_mgmt_hal Initialize----->Entry\n");
+        return TEST_SUCCESS;
+}
+
+/*******************************************************************************************
+ *
+ * Function Name    : mso_mgmt_hal_GetMsoPodSeed
+ * Description      : This will get the MSO POD Seed value
+ *
+
+ * @param [in]  req - paramType : Holds NULL in the case of negative scenario and empty otherwise
+ * @param [out] response - filled with SUCCESS or FAILURE based on the return value
+ *
+ *******************************************************************************************/
+
+void mso_mgmt_hal::mso_mgmt_hal_GetMsoPodSeed(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE,"\n mso_mgmt_hal_GetMsoPodSeed  --->Entry \n");
+
+    int returnValue = 0;
+    char Details[800] = {'\0'};
+    char value[700] = {'\0'};
+    char paramType[10] = {'\0'};
+	
+    strcpy(paramType, req["paramType"].asCString());
+
+    //For negative scenario, "NULL" will be passed as the paramType argument
+    if(strcmp(paramType, "NULL"))
+        returnValue = ssp_mso_mgmt_hal_GetMsoPodSeed(value);
+    else
+       returnValue = ssp_mso_mgmt_hal_GetMsoPodSeed(NULL);
+    if(0 == returnValue)
+    {
+       sprintf(Details,"%s", value);
+       DEBUG_PRINT(DEBUG_TRACE,"\n MSOPODSeed: %s\n",value);
+       response["result"]="SUCCESS";
+       response["details"]=Details;
+    }
+    else
+    {
+       response["result"]="FAILURE";
+       DEBUG_PRINT(DEBUG_TRACE,"\n MSOPODSeed: %s\n",value);
+       response["details"]="Failed to get the value";
+       DEBUG_PRINT(DEBUG_TRACE,"\n mso_mgmt_hal_GetMsoPodSeed --->Exit\n");
+       return;
+    }
+    DEBUG_PRINT(DEBUG_TRACE,"\n mso_mgmt_hal_GetMsoPodSeed --->Exit\n");
+    return;
+}
+
+/*******************************************************************************************
+ *
+ * Function Name    : mso_mgmt_hal_MsoValidatePwd
+ * Description      : This will validate the password
+ *
+
+ * @param [in]  req - paramValue: Holds the password 
+                      paramType : Holds NULL in the case of negative scenario and empty otherwise
+ * @param [out] response - filled with SUCCESS or FAILURE based on the return value
+ *
+ *******************************************************************************************/
+
+void mso_mgmt_hal::mso_mgmt_hal_MsoValidatePwd(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE,"\n mso_mgmt_hal_MsoValidatePwd  --->Entry \n");
+
+    int returnValue = 0;
+    char Details[100] = {'\0'};
+    char paramValue[10] = {'\0'};
+    char paramType[10] = {'\0'};
+    char output[20] = {'\0'};
+
+    /* Validate the input arguments */
+    strcpy(paramType, req["paramType"].asCString());
+    strcpy(paramValue, req["paramValue"].asCString());
+
+
+    //For negative scenario, "NULL" will be passed as the paramType argument
+    if(strcmp(paramType, "NULL"))
+        returnValue = ssp_mso_mgmt_hal_MsoValidatePwd(paramValue,output);
+    else
+       returnValue = ssp_mso_mgmt_hal_MsoValidatePwd(NULL,output);
+    if(0 == returnValue)
+    {
+       sprintf(Details,"%s", output);
+       DEBUG_PRINT(DEBUG_TRACE,"MSO Password validation value is %s\n",output);
+       response["result"]="SUCCESS";
+       response["details"]=Details;
+    }
+    else
+    {
+       response["result"]="FAILURE";
+       response["details"]="Failed to validate the password";
+       DEBUG_PRINT(DEBUG_TRACE,"MSO Password validation value is %s\n",output);      
+    }
+    DEBUG_PRINT(DEBUG_TRACE,"\n mso_mgmt_hal_MsoValidatePwd --->Exit\n");
+    return;
+
+}
+
+/***************************************************************************************************
+ *Function Name   : CreateObject
+ *Description     : This function is used to create a new object of the class "msomgmthal".
+ *@param [in]     : None
+ ***************************************************************************************************/
+extern "C" mso_mgmt_hal* CreateObject(TcpSocketServer &ptrtcpServer)
+{
+        return new mso_mgmt_hal(ptrtcpServer);
+}
+
+/*************************************************************************************
+ *Function Name : cleanup
+ *Description   : This function will be used to the close things cleanly.
+ *@param [in]   : szVersion - version, ptrAgentObj - Agent object
+ *@param [out]  : response - filled with SUCCESS or FAILURE based on the return value
+ **************************************************************************************/
+bool mso_mgmt_hal::cleanup(IN const char* szVersion)
+{
+        DEBUG_PRINT(DEBUG_TRACE, "cleaning up\n");
+        return TEST_SUCCESS;
+}
+
+/**********************************************************************************
+ *Function Name : DestroyObject
+ *Description   : This function will be used to destroy the mso_mgmt_hal object.
+ *@param [in]   : Input argument is mso_mgmt_hal Object
+ **********************************************************************************/
+extern "C" void DestroyObject(mso_mgmt_hal *stubobj)
+{
+        DEBUG_PRINT(DEBUG_TRACE, "Destroying mso_mgmt_hal object\n");
+        delete stubobj;
+}
+
+
+
