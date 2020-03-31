74c28914c61a1b38e260a38859c431cda4800d35
Raksha <raksha_vaidya@comcast.com>
2020-03-31 05:13:28 +0000

    TDKB-460 : Script modification for TS_MTAAGENT_SetAttr_RootDevice
    Reason for change: Adding check for Device.X_CISCO_COM_MTA.DSXLogNumberOfEntries
    Test Procedure: Run scripts via TDK TM
    Risks: None
    Signed-off-by: raksha vaidya <raksha.vaidya@tataelxsi.co.in>
    
    (cherry picked from commit fe724dd4c992ac8375882a37fabfda73187e0d9e)
    Change-Id: I31aa0288165a9c147b4f8df774c4568f12317613

--- testscripts/RDKB/component/MTA_Agent/TS_MTAAGENT_SetAttr_RootDevice.py
+++ testscripts/RDKB/component/MTA_Agent/TS_MTAAGENT_SetAttr_RootDevice.py
@@ -50,18 +50,18 @@ API Name
 MTA_agent_SetParameterAttr
 Input
 1.PathName ( eg: "Device." )
-2.Notification type 
-("notification") 
+2.Notification type
+("notification")
 (eg:pasive)
-3.Access Control Change type ("accessControlChanged") 
+3.Access Control Change type ("accessControlChanged")
 (eg:anybody)</input_parameters>
     <automation_approch>1.Create a function named MTA_agent_SetParameterAttr in Test Manager GUI.
 2.Configure the info of the  function under test in function  and create a python script
-3.Execute the generated Script(TS_MTAAGENT_SetAttr_RootDevice.py) using execution page of  Test Manager GUI 
-4.This script will in turn call mta_agent stub in TDK Agent 
+3.Execute the generated Script(TS_MTAAGENT_SetAttr_RootDevice.py) using execution page of  Test Manager GUI
+4.This script will in turn call mta_agent stub in TDK Agent
 5.MTA_agent_SetParameterAttr function will call CCSP Base Interface Function named "CcspBaseIf_setParameterAttributes" , that inturn will call MTA Agent Library Function "CcspCcMbi_SetParameterAttributes" along with provided path name and Value
 6.MTA_agent_SetParameterAttr  function will call CCSP Base Interface Function named "CcspBaseIf_getParameterAttributes" for the same inputs to check whether the attributes have been updated .
-7.Responses(printf) from TDK Component and mta agentstub would be logged in Agent Console log 
+7.Responses(printf) from TDK Component and mta agentstub would be logged in Agent Console log
 8.Based on the log set the result as SUCCESS or FAILURE</automation_approch>
     <except_output>CheckPoint 1:
 Attributes of Requested Path is compared with the the given input values
@@ -80,68 +80,85 @@ TestManager GUI will publish the result as PASS in Execution page</except_output
 </xml>
 
 '''
-																								# use tdklib library,which provides a wrapper for tdk testcase script 
-import tdklib; 
+#use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
 
 #Test component to be tested
 obj = tdklib.TDKScriptingLibrary("Mta_agent","RDKB");
-
+obj1 = tdklib.TDKScriptingLibrary("tdkbtr181","1");
 #IP and Port of box, No need to change,
 #This will be replaced with corresponding Box Ip and port while executing script
 ip = <ipaddress>
 port = <port>
 obj.configureTestCase(ip,port,'TS_MTAAGENT_SetAttr_RootDevice');
-
+obj1.configureTestCase(ip,port,'TS_MTAAGENT_SetAttr_RootDevice');
 #Get the result of connection with test component and STB
 loadmodulestatus =obj.getLoadModuleResult();
+obj1loadmodulestatus =obj1.getLoadModuleResult();
 print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
-
-if "SUCCESS" in loadmodulestatus.upper():
+print "[LIB LOAD STATUS]  :  %s" %obj1loadmodulestatus ;
+if "SUCCESS" in loadmodulestatus.upper() and obj1loadmodulestatus.upper():
     #Set the result status of execution
     obj.setLoadModuleStatus("SUCCESS");
-		
-    tdkTestObj = obj.createTestStep('MTA_agent_SetParameterAttr');  
-    tdkTestObj.addParameter("ParamName","Device.X_CISCO_COM_MTA.DSXLog.");
-    tdkTestObj.addParameter("AccessControl","anybody");
-    tdkTestObj.addParameter("Notify","active");
-    expectedresult="SUCCESS";
+    obj1.setLoadModuleStatus("SUCCESS");
 
+    tdkTestObj = obj1.createTestStep('TDKB_TR181Stub_Get');
+    tdkTestObj.addParameter("ParamName","Device.X_CISCO_COM_MTA.DSXLogNumberOfEntries");
+    expectedresult="SUCCESS";
     #Execute the test case in STB
     tdkTestObj.executeTestCase(expectedresult);
     actualresult = tdkTestObj.getResult();
     details = tdkTestObj.getResultDetails();
-    if expectedresult in actualresult:
-        #Set the result status of execution
-        tdkTestObj.setResultStatus("SUCCESS");
-        details = tdkTestObj.getResultDetails();
-        print "TEST STEP 1:Set the notification attribute of DSX Log parameters to Anybody";
-        print "EXPECTED RESULT 1: Should Successfully set the attributes";
-        print "ACTUAL RESULT 1: %s" %details;
-        print "[TEST EXECUTION RESULT] : %s" %actualresult ;
-	print "%s" %details;
-	 
-    else:   
-        tdkTestObj.setResultStatus("FAILURE");
-        details = tdkTestObj.getResultDetails();
-        print "TEST STEP 1:Set the notification attribute of DSX Log parameters to Anybody";
-        print "EXPECTED RESULT 1: Should Successfully set the attributes";
-        print "ACTUAL RESULT 1: %s" %details;
-	print "[TEST EXECUTION RESULT] : %s" %actualresult ;	
-        print "%s" %details;
+    print "details: ",details
+    numEntries = int(details);
+    if expectedresult in actualresult and numEntries  > 0:
+       tdkTestObj = obj.createTestStep('MTA_agent_SetParameterAttr');
+       tdkTestObj.addParameter("ParamName","Device.X_CISCO_COM_MTA.DSXLog.");
+       tdkTestObj.addParameter("AccessControl","anybody");
+       tdkTestObj.addParameter("Notify","active");
+       expectedresult="SUCCESS";
 
+       #Execute the test case in STB
+       tdkTestObj.executeTestCase(expectedresult);
+       actualresult = tdkTestObj.getResult();
+       details = tdkTestObj.getResultDetails();
+       if expectedresult in actualresult:
+          #Set the result status of execution
+          tdkTestObj.setResultStatus("SUCCESS");
+          details = tdkTestObj.getResultDetails();
+          print "TEST STEP 1:Set the notification attribute of DSX Log parameters to Anybody";
+          print "EXPECTED RESULT 1: Should Successfully set the attributes";
+          print "ACTUAL RESULT 1: %s" %details;
+          print "[TEST EXECUTION RESULT] : %s" %actualresult ;
+	  print "%s" %details;
+
+       else:
+           tdkTestObj.setResultStatus("FAILURE");
+           details = tdkTestObj.getResultDetails();
+           print "TEST STEP 1:Set the notification attribute of DSX Log parameters to Anybody";
+           print "EXPECTED RESULT 1: Should Successfully set the attributes";
+           print "ACTUAL RESULT 1: %s" %details;
+           print "[TEST EXECUTION RESULT] : %s" %actualresult ;
+           print "%s" %details;
+    else:
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "Since DSXLog Number Of Entries is %s need not query Device.X_CISCO_COM_MTA.DSXLog. " %details
+        print "[TEST EXECUTION RESULT] : SUCCESS" ;
     obj.unloadModule("Mta_agent");
-   		 
-else:   
+    obj1.unloadModule("tdkbtr181");
+
+else:
     print "Failed to load MTA module";
     obj.setLoadModuleStatus("FAILURE");
-    print "Module loading failed";				
-				
-				
+    print "Module loading failed";
 
-					
 
-					
 
-					
 
-					
+
+
+
+
+
+
+
