2b28aa03fe6904d7ea503352ea28055f10876021
Shakul Hameed <ShakulHameed_MeeyanMydeen@comcast.com>
2020-07-28 18:43:20 +0000

    TDKB-1260: M79 Coverage Improvement
    Reason for change: Adding new scripts for TR181 and HAL Value comparison framework
    Test Procedure: Run scripts via TDK TM
    Risks: None
    Signed-off-by: Shakul Hameed <ShakulHameed_MeeyanMydeen@comcast.com>
    
    (cherry picked from commit 8a9efc829cdfaa9557c507b35972eaa98f2630a9)
    Change-Id: Ia1ea0fb86f2d8c445279d5b206dff9b472065f05

---
+++ testscripts/RDKB/component/TDKB_HAL/TDKB_MTAHAL_SNMP_GetAllValues.py
@@ -0,0 +1,139 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>17</version>
+  <name>TDKB_MTAHAL_SNMP_GetAllValues</name>
+  <primitive_test_id/>
+  <primitive_test_name>TDKB_HALStub_Get</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>Check if all TR181 parameter values of MTA (Using SNMP) matching with its corresponding HAL API Values</synopsis>
+  <groups_id/>
+  <execution_time>20</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>Broadband</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TDKB_HAL_4</test_case_id>
+    <test_objective>To Check if all TR181 parameter values of MTA (Using SNMP) matching with its corresponding HAL API Values</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>TDK test agent should be running
+MTA HAL module's parameter xml should be available</pre_requisite>
+    <api_or_interface_used>none</api_or_interface_used>
+    <input_parameters>none</input_parameters>
+    <automation_approch>1. Load tdkbtr181 module, MTAHAL module and sysutil module
+2. Set the validation type as SNMP
+3. Invoke prerequisite() by passing setup type as SNMP and make sure all prerequisite are met
+4. Invoke parseXML() by passing MTAHAL as module name and get the Parameters and HAL API list
+5. Invoke getTR181andHALAPIValue() by passing MTAHAL as module name
+6. Check if all TR181 parameters values are matching with HAL API values
+7. Display the final status of get and the list of failed parameters, if any
+8. Unload modules</automation_approch>
+    <expected_output>All TR181 parameter values should match with its corresponding HAL API Values
+</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>tdkbhal</test_stub_interface>
+    <test_script>TDKB_MTAHAL_SNMP_GetAllValues</test_script>
+    <skipped>No</skipped>
+    <release_version>M79</release_version>
+    <remarks>none</remarks>
+  </test_cases>
+  <script_tags/>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from time import sleep;
+import xml.etree.ElementTree as ET
+import os
+from tdkbVariables import *;
+import webpaUtility;
+from webpaUtility import *;
+import tdkbHalTr181Utility;
+
+objhal = tdklib.TDKScriptingLibrary("mtahal","1");
+obj = tdklib.TDKScriptingLibrary("tdkbtr181","1");
+sysobj = tdklib.TDKScriptingLibrary("sysutil","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+objhal.configureTestCase(ip,port,'TDKB_MTAHAL_SNMP_GetAllValues');
+obj.configureTestCase(ip,port,'TDKB_MTAHAL_SNMP_GetAllValues');
+sysobj.configureTestCase(ip,port,'TDKB_MTAHAL_SNMP_GetAllValues');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus1 = objhal.getLoadModuleResult();
+loadmodulestatus2 = obj.getLoadModuleResult();
+loadmodulestatus3 = sysobj.getLoadModuleResult();
+print "[MTA HAL LIB LOAD STATUS]  :  %s " %loadmodulestatus1 ;
+print "[TDKBTR181 LIB LOAD STATUS]  :  %s " %loadmodulestatus2 ;
+print "[SYSUTIL LIB LOAD STATUS]  :  %s " %loadmodulestatus3 ;
+
+if "SUCCESS" in (loadmodulestatus1.upper() and loadmodulestatus2.upper() and loadmodulestatus3.upper()):
+    #Set the result status of execution
+    objhal.setLoadModuleStatus("SUCCESS");
+    obj.setLoadModuleStatus("SUCCESS");
+    sysobj.setLoadModuleStatus("SUCCESS");
+
+    setup_type = "SNMP"
+    tdkTestObj = sysobj.createTestStep('ExecuteCmd');
+
+    prerequisite_status = tdkbHalTr181Utility.prerequisite(sysobj,setup_type);
+    if prerequisite_status == 1:
+        print "Pre-requisite for Setup Type %s is SUCCESS"%setup_type
+
+        xmlName = tdkbHalTr181Utility.getXMLNameUsingDeviceType(sysobj,"MTAHAL");
+
+        if xmlName != "Error":
+            paramList,HALAPIList,tr181_ExpRes,hal_ExpRes = tdkbHalTr181Utility.parseXML(xmlName,"MTAHAL",setup_type);
+
+            for plist in range (0,len(paramList)):
+                mainFunctionFailedParams = tdkbHalTr181Utility.getTR181andHALAPIValue(obj,objhal,sysobj,paramList[plist],HALAPIList[plist],tr181_ExpRes[plist],hal_ExpRes[plist],"MTAHAL",setup_type);
+
+            print "Failed Parameters are %s"%mainFunctionFailedParams;
+
+        else:
+            print "Failed to get the XML Name"
+            tdkTestObj.setResultStatus("FAILURE");
+    else:
+        print "Pre-requisite Failed";
+        tdkTestObj.setResultStatus("FAILURE");
+
+    obj.unloadModule("tdkbtr181");
+    sysobj.unloadModule("sysutil");
+    objhal.unloadModule("mtahal");
+else:
+    print "Failed to load MTAHAL/TDKB-TR181 module";
+    obj.setLoadModuleStatus("FAILURE");
+    objhal.setLoadModuleStatus("FAILURE")
+    sysobj.setLoadModuleStatus("FAILURE")
+    print "Module loading failed";
