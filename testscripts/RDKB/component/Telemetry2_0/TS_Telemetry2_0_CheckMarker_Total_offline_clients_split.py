7dd41e4807d16c0f06422cd9496c88d5aa8a9fca
Raksha <raksha_vaidya@comcast.com>
2020-08-19 13:05:41 +0000

    TDKB-1330: M80 Coverage Improvement
    Reason for change: Adding new scripts for Telemerty2.0
    Test Procedure: Run scripts via TDK TM.
    Risks: None
    Signed-off-by: Raksha  <raksha.vaidya@tataelxsi.co.in>
    
    (cherry picked from commit 6dbb94f711deafca757c0a91f52939f5d59c0e0d)
    Change-Id: Iabbdf2c67f3f49971f3bf3e5c8fd6db8d511b8da

---
+++ testscripts/RDKB/component/Telemetry2_0/TS_Telemetry2_0_CheckMarker_Total_offline_clients_split.py
@@ -0,0 +1,245 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>4</version>
+  <name>TS_Telemetry2_0_CheckMarker_Total_offline_clients_split</name>
+  <primitive_test_id/>
+  <primitive_test_name>Telemetry_DoNothing</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>To simulate Total_offline_clients_split marker by restarting the ccsp-lmlite process</synopsis>
+  <groups_id/>
+  <execution_time>40</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>Broadband</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TELEMETRY2_0_5</test_case_id>
+    <test_objective>This test case is to simulate Total_offline_clients_split marker by restarting the ccsp-lmlite process</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>1.Ccsp Components  should be in a running state of DUT
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>N/A</api_or_interface_used>
+    <input_parameters>Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Enable
+Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.Version
+Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Telemetry.ConfigURL</input_parameters>
+    <automation_approch>1. Load the tr181 and sysutil Modules
+2. Initiate the Telemetry2_0 Pre requisite function from telemetry2_0 library, The function should return success along with revert flag and initial values
+3. Get the number of lines from the telemetry log file and store the value
+4. Restart the CcspLMLite  process
+5. Get the number of lines from the telemetry log file after simulation
+6. Check if marker Total_offline_clients_split  is present in  telemetry log file (only between the initial line count and line count after simulation)
+7. Make the script as success if the Marker is present else make it failure
+8. Initiate  telemetry post process for telemetry2_0 (to check if any revert operation required) and the function should return success
+9. Unload the modules
+</automation_approch>
+    <expected_output>On simulation by restarting the CcspLMLite process Total_offline_clients_split  marker should be present.</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>Telemetry2_0</test_stub_interface>
+    <test_script>TS_Telemetry2_0_CheckMarker_Total_offline_clients_split</test_script>
+    <skipped>No</skipped>
+    <release_version>M80</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+  <script_tags/>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from tdkbTelemetry2_0Utility import *;
+from time import sleep;
+
+#Test component to be tested
+sysobj = tdklib.TDKScriptingLibrary("sysutil","1");
+tr181obj = tdklib.TDKScriptingLibrary("tdkbtr181","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+sysobj.configureTestCase(ip,port,'TS_Telemetry2_0_CheckMarker_Total_offline_clients_split');
+tr181obj.configureTestCase(ip,port,'TS_Telemetry2_0_CheckMarker_Total_offline_clients_split');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus=sysobj.getLoadModuleResult();
+loadmodulestatus1=tr181obj.getLoadModuleResult();
+
+if "SUCCESS" in loadmodulestatus.upper() and "SUCCESS" in loadmodulestatus1.upper() :
+    #Set the result status of execution
+    sysobj.setLoadModuleStatus("SUCCESS");
+    tr181obj.setLoadModuleStatus("SUCCESS");
+    expectedresult="SUCCESS";
+
+    tdkTestObj_Sys_ExeCmd = sysobj.createTestStep('ExecuteCmd');
+    tdkTestObj_Tr181_Get = tr181obj.createTestStep('TDKB_TR181Stub_Get');
+    tdkTestObj_Tr181_set = tr181obj .createTestStep('TDKB_TR181Stub_Set');
+
+    revertFlag = 0;
+    initialStatus = "";
+    initialVersion = "";
+    initialURL = "";
+
+    print "***************************************************************"
+    print "TEST STEP 1: Initiating Pre-Requisite Check for Telemetry2_0";
+    print "EXPECTED RESULT 1:Pre-Requisite Check for Telemetry2_0 Should be Success";
+
+    preReq_Status,revertFlag,initialStatus,initialVersion,initialURL = telemetry2_0_Prerequisite(sysobj,tdkTestObj_Sys_ExeCmd,tdkTestObj_Tr181_Get,tdkTestObj_Tr181_set);
+
+    if preReq_Status == 1:
+        print "ACTUAL RESULT 1: Pre-Requisite for Telemetry2_0 was successful";
+        print "[TEST EXECUTION RESULT] : SUCCESS";
+        print "***************************************************************"
+        tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+
+        lineCountResult, initialLinesCount = getTelLogFileTotalLinesCount(tdkTestObj_Sys_ExeCmd);
+
+        if expectedresult in lineCountResult:
+            print "Initial Line count of Telemetry Log File is ",initialLinesCount
+            tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+            print "TEST STEP 2: Get the initial Line count of Telemetry Log file";
+            print "EXPECTED RESULT 2 : Should get the initial line count of Telemetry Log file";
+            print "ACTUAL RESULT 2: Line count retrieved Successfully";
+            print "[TEST EXECUTION RESULT] : SUCCESS";
+
+            details,actualresult = getPID(tdkTestObj_Sys_ExeCmd,"CcspLMLite");
+            if expectedresult in actualresult and details != "":
+               Initialpid = int(details);
+               tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+               print "TEST STEP 3: Check if CcspLMLite process is running";
+               print "EXPECTED RESULT 3:CcspLMLite  process should be running";
+               print "ACTUAL RESULT 3: pid of CcspLMLite:",details;
+               print "[TEST EXECUTION RESULT] : SUCCESS";
+
+               actualresult =killProcess(tdkTestObj_Sys_ExeCmd,Initialpid,"");
+               if expectedresult in actualresult :
+                  tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+                  print "TEST STEP 4: Kill CcspLMLite process and run task_health_monitor.sh script";
+                  print "EXPECTED RESULT 4:Should Kill the CcspLMLite  process and run task_health_monitor.sh script";
+                  print "ACTUAL RESULT 4: CcspLMLite process killed and run task_health_monitor.sh script successfully";
+                  print "[TEST EXECUTION RESULT] : SUCCESS";
+
+                  sleep(100);
+
+                  lineCountResult1, lineCountAfterSimu = getTelLogFileTotalLinesCount(tdkTestObj_Sys_ExeCmd);
+                  if expectedresult in lineCountResult and  int(lineCountAfterSimu) > int(initialLinesCount):
+                     print "Line count of Telemetry Log File After Simulation is ",lineCountAfterSimu
+                     tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+                     print "TEST STEP 5: Get the line count of telemetry log file and compare the value with initialLinesCount";
+                     print "EXPECTED RESULT 5: Line count After Simulation should be greater than the initialLinesCount";
+                     print "ACTUAL RESULT 5: Line count After Simulation is greater than the initialLinesCount";
+                     print "[TEST EXECUTION RESULT] : SUCCESS";
+
+                     query = "sed -n -e %s,%sp /rdklogs/logs/telemetry2_0.txt.0 | grep -i \"Received eventInfo : Total_offline_clients_split\"" %(initialLinesCount,lineCountAfterSimu)
+                     print "query:%s" %query
+                     tdkTestObj_Sys_ExeCmd.addParameter("command", query);
+                     tdkTestObj_Sys_ExeCmd.executeTestCase(expectedresult);
+                     actualresult = tdkTestObj_Sys_ExeCmd.getResult();
+                     details = tdkTestObj_Sys_ExeCmd.getResultDetails().strip().replace("\\n","");
+                     print "Marker Detail Found from log file is: %s "%details;
+
+                     if expectedresult in actualresult and details!="" and (len(details) > 0) and "Total_offline_clients_split" in details:
+                        tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+                        markervalue = details.split("Total_offline_clients_split<#=#>")[1]
+                        print "TEST STEP 6:Total_offline_clients_split  Marker should be present";1
+                        print "EXPECTED RESULT 6: Total_offline_clients_split Marker should be present";
+                        print "ACTUAL RESULT 6: Total_offline_clients_split  Marker Value is %s" %markervalue;
+                        #Get the result of execution
+                        print "[TEST EXECUTION RESULT] : SUCCESS";
+
+                        actualresult,pid = checkProcessRestarted(tdkTestObj_Sys_ExeCmd,"CcspLMLite");
+                        if expectedresult in actualresult and pid != "" and pid != Initialpid:
+                           tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+                           print "TEST STEP 7: Check if the CcspLMLite proccess restarted";
+                           print "EXPECTED RESULT 7:CcspLMLite proccess should restart ";
+                           print "ACTUAL RESULT 7: CcspLMLite restarted successfully";
+                           print "[TEST EXECUTION RESULT] : SUCCESS";
+                        else:
+                            tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+                            print "TEST STEP 7: Check if the CcspLMLite proccess restarted";
+                            print "EXPECTED RESULT 7:CcspLMLite proccess should restart ";
+                            print "ACTUAL RESULT 7: CcspLMLite restarted failed";
+                            print "[TEST EXECUTION RESULT] : FAILURE";
+                     else:
+                         tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+                         print "TEST STEP 6:Total_offline_clients_split  Marker should be present";
+                         print "EXPECTED RESULT 6: Total_offline_clients_split Marker should be present";
+                         print "ACTUAL RESULT 6: Total_offline_clients_split  Marker is %s" %details;
+                         #Get the result of execution
+                         print "[TEST EXECUTION RESULT] : FAILURE";
+                  else:
+                      tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+                      print "TEST STEP 5: Get the line count of telemetry log file and compare the value with initialLinesCount";
+                      print "EXPECTED RESULT 5: Line count After Simulation should be greater than the initialLinesCount";
+                      print "ACTUAL RESULT 5: Line count After Simulation is NOT greater than the initialLinesCount";
+                      print "[TEST EXECUTION RESULT] : FAILURE";
+               else:
+                   tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+                   print "TEST STEP 4: Kill CcspLMLite process and run task_health_monitor.sh script";
+                   print "EXPECTED RESULT 4:Should Kill the CcspLMLite  process and run task_health_monitor.sh script";
+                   print "ACTUAL RESULT 4: Failed to kill CcspLMLite process and run task_health_monitor.sh script";
+                   print "[TEST EXECUTION RESULT] : FAILURE";
+            else:
+                tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+                print "TEST STEP 3: Check if CcspLMLite process is running";
+                print "EXPECTED RESULT 3:CcspLMLite  process should be running";
+                print "ACTUAL RESULT 3: pid of CcspLMLite:",details;
+                print "[TEST EXECUTION RESULT] : FAILURE";
+        else:
+            tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+            print "TEST STEP 2: Get the initial Line count of Telemetry Log file";
+            print "EXPECTED RESULT 2 : Should get the initial line count of Telemetry Log file";
+            print "ACTUAL RESULT 2: Failed to retrive Line count";
+            print "[TEST EXECUTION RESULT] : FAILURE";
+    else:
+        tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+        print "ACTUAL RESULT 1: Pre-Requisite for Telemetry2_0 was Failed";
+        print "[TEST EXECUTION RESULT] : FAILURE";
+        print "***************************************************************"
+
+    print "***************************************************************"
+    print "TEST STEP 8: Initiating Post Process for Telemetry2_0";
+    print "EXPECTED RESULT 8: Post Process should be success";
+
+    postprocess_Status = telemetry2_0_PostProcess(sysobj,tdkTestObj_Sys_ExeCmd,tdkTestObj_Tr181_set,revertFlag,initialStatus,initialVersion,initialURL);
+    if postprocess_Status == 1:
+        tdkTestObj_Sys_ExeCmd.setResultStatus("SUCCESS");
+        print "ACTUAL RESULT 8 : Post Process for Telemetry2_0 was Successful";
+        print "[TEST EXECUTION RESULT] : SUCCESS";
+    else:
+        tdkTestObj_Sys_ExeCmd.setResultStatus("FAILURE");
+        print "ACTUAL RESULT 8: Post Process for Telemetry2_0 was Failed";
+        print "[TEST EXECUTION RESULT] : FAILURE";
+
+    tr181obj.unloadModule("tdkbtr181");
+    sysobj.unloadModule("sysutil");
+else:
+    print "Failed to load module";
+    sysobj.setLoadModuleStatus("FAILURE");
+    tr181obj.setLoadModuleStatus("FAILURE");
