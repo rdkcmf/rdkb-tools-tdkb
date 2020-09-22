6ace2400215f12dcd8409a15cb3249356a37be96
svelus003c <sugasini_velusamy@comcast.com>
2020-09-22 02:31:21 +0000

    TDKB-1396 : Remove the logic to check the image name
    Reason for change: Image name of RPI and EMU will not have
    the keyword TDK.
    Test Procedure: Check whether TDK is up and running during
    bootup
    Risks: None
    Signed-off-by: svelus003c <sugasini_velusamy@comcast.com>
    
    (cherry picked from commit 8ec8b49bb1ef1fc77de951f204634d5c95326ca0)
    Change-Id: I2808f5ccedf77b099819d036085b345568832bc5

--- agent/scripts/tdkb_launcher.sh
+++ agent/scripts/tdkb_launcher.sh
@@ -28,70 +28,58 @@ APP_NAME=tdk-b-dl
 VALIDATION_METHOD=openssl
 PKG_EXT=ipk
 
-if [ -f /version.txt ]; then
-        echo "version.txt exists" >> $LOG_FILE
-        imgname=`cat /version.txt | grep -i 'imagename'`
-        echo $imgname >> $LOG_FILE
-        flag=`echo $imgname|awk '{print match($0,"TDK")}'`;
-        if [ $flag -gt 0 ]; then
-                if [ -f "$TDK_AGENT_PATH/StartTDK.sh" ]; then
-                        echo "Found TDK-B Image but not RDM enabled" >> $LOG_FILE
-                        echo "Invoking StartTDK.sh from $TDK_AGENT_PATH" >> $LOG_FILE
-                        sh $TDK_AGENT_PATH/StartTDK.sh
+if [ -f "$TDK_AGENT_PATH/StartTDK.sh" ]; then
+        echo "Found TDK-B Image but not RDM enabled" >> $LOG_FILE
+        echo "Invoking StartTDK.sh from $TDK_AGENT_PATH" >> $LOG_FILE
+        sh $TDK_AGENT_PATH/StartTDK.sh
 
-                elif [ -f "$TDK_AGENT_RDM_PATH/StartTDK.sh" ]; then
-                        echo "Found RDM enabled TDK-B Image" >> $LOG_FILE
+elif [ -f "$TDK_AGENT_RDM_PATH/StartTDK.sh" ]; then
+        echo "Found RDM enabled TDK-B Image" >> $LOG_FILE
+        echo "Invoking StartTDK.sh from $TDK_AGENT_RDM_PATH" >> $LOG_FILE
+        sh $TDK_AGENT_RDM_PATH/StartTDK.sh
+
+else
+        count=0
+        while [ "$count" -lt "12" ]
+        do
+                echo "Loop Count: $count" >> $LOG_FILE
+                if [ -f /tmp/.xconfssrdownloadurl ]; then
+                        echo "File /tmp/.xconfssrdownloadurl is present...." >> $LOG_FILE
+                        url_status=`cat /tmp/.xconfssrdownloadurl`
+                        if [[ $url_status =~ .*https.* ]]; then
+                                echo "URL found in /tmp/.xconfssrdownloadurl...." >> $LOG_FILE
+                                break
+                        else
+                                echo "URL not found in /tmp/.xconfssrdownloadurl...." >> $LOG_FILE
+                                echo "Waiting for another 60 seconds!!!!!!"
+                                sleep 60
+                        fi
+                else
+                        echo "File /tmp/.xconfssrdownloadurl is not present...." >> $LOG_FILE
+                        sleep 60
+                fi
+                        count=`expr $count + 1`
+        done
+
+        echo "Download the TDK-B package" >> $LOG_FILE
+        sh $DNLD_SCRIPT $APP_NAME "" $VALIDATION_METHOD $PKG_EXT ""
+        DNLD_RES=$?
+        echo "TDK-B package download result is: $DNLD_RES" >> $LOG_FILE
+
+        if [ $DNLD_RES -eq 0 ]; then
+                echo "TDK-B package download is successful" >> $LOG_FILE
+                echo "Sleep for 60 seconds for the package to untar...." >> $LOG_FILE
+                sleep 60
+                export PATH=$PATH:$TDK_AGENT_RDM_PATH
+                export LD_LIBRARY_PATH=/lib:/usr/lib:$TDK_AGENT_LIB_PATH
+                if [ -f "$TDK_AGENT_RDM_PATH/StartTDK.sh" ]; then
                         echo "Invoking StartTDK.sh from $TDK_AGENT_RDM_PATH" >> $LOG_FILE
                         sh $TDK_AGENT_RDM_PATH/StartTDK.sh
-
                 else
-                        count=0
-                        while [ "$count" -lt "12" ]
-                        do
-                                echo "Loop Count: $count" >> $LOG_FILE
-                                if [ -f /tmp/.xconfssrdownloadurl ]; then
-                                        echo "File /tmp/.xconfssrdownloadurl is present...." >> $LOG_FILE
-                                        url_status=`cat /tmp/.xconfssrdownloadurl`
-                                        if [[ $url_status =~ .*https.* ]]; then
-                                                echo "URL found in /tmp/.xconfssrdownloadurl...." >> $LOG_FILE
-                                                break
-                                        else
-                                                echo "URL not found in /tmp/.xconfssrdownloadurl...." >> $LOG_FILE
-                                                echo "Waiting for another 60 seconds!!!!!!"
-                                                sleep 60
-                                        fi
-                                else
-                                        echo "File /tmp/.xconfssrdownloadurl is not present...." >> $LOG_FILE
-                                        sleep 60
-                                fi
-                                count=`expr $count + 1`
-                        done
-
-                        echo "Download the TDK-B package" >> $LOG_FILE
-                        sh $DNLD_SCRIPT $APP_NAME "" $VALIDATION_METHOD $PKG_EXT ""
-                        DNLD_RES=$?
-                        echo "TDK-B package download result is: $DNLD_RES" >> $LOG_FILE
-
-                        if [ $DNLD_RES -eq 0 ]; then
-                                echo "TDK-B package download is successful" >> $LOG_FILE
-				echo "Sleep for 60 seconds for the package to untar...." >> $LOG_FILE
-				sleep 60
-                                export PATH=$PATH:$TDK_AGENT_RDM_PATH
-                                export LD_LIBRARY_PATH=/lib:/usr/lib:$TDK_AGENT_LIB_PATH
-                                if [ -f "$TDK_AGENT_RDM_PATH/StartTDK.sh" ]; then
-                                       echo "Invoking StartTDK.sh from $TDK_AGENT_RDM_PATH" >> $LOG_FILE
-                                       sh $TDK_AGENT_RDM_PATH/StartTDK.sh
-                                else
-                                        echo "TDK-B startup script is not available under $TDK_AGENT_RDM_PATH" >> $LOG_FILE
-                                fi
-                        else
-                                echo "TDK-B Download failed!!! result is: $DNLD_RES" >> $LOG_FILE
-                        fi
+                        echo "TDK-B startup script is not available under $TDK_AGENT_RDM_PATH" >> $LOG_FILE
                 fi
         else
-                echo "Current image is not TDK-B Image" >> $LOG_FILE
+                echo "TDK-B Download failed!!! result is: $DNLD_RES" >> $LOG_FILE
         fi
-else
-        echo "version.txt file is missing" >> $LOG_FILE
 fi
 
