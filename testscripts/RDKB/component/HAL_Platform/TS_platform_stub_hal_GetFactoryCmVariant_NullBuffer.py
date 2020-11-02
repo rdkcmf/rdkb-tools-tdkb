82c4de8878f5cf922527e9535f446e29508cc24f
ssurendranpillai <smrithis@tataelxsi.co.in>
2020-11-02 14:24:49 +0000

    TDKB-1459 : M82 HAL test coverage
    Reason for change: Enhance ethsw_hal nad platform_hal test coverage
    Test Procedure: Run ethsw_hal and platform_hal scripts via TDK TM.
    Risks: None
    Signed-off-by: Smrithi <smrithis@tataelxsi.co.in>
    
    (cherry picked from commit 8374162c9482c56d3cb1d05e5679dfdb621208d8)
    Change-Id: I3bc9d59f440e87b990bff407cb09c09af5a6725c

---
+++ testscripts/RDKB/component/HAL_Platform/TS_platform_stub_hal_GetFactoryCmVariant_NullBuffer.py
@@ -0,0 +1,109 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>1</version>
+  <name>TS_platform_stub_hal_GetFactoryCmVariant_NullBuffer</name>
+  <primitive_test_id/>
+  <primitive_test_name>platform_stub_hal_getFactoryCmVariant</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>To validate HAL API platform_hal_getFactoryCmVariant() by passing a Null buffer</synopsis>
+  <groups_id/>
+  <execution_time>5</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>Broadband</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_HAL_Platform_73</test_case_id>
+    <test_objective>To validate HAL API platform_hal_getFactoryCmVariant() by passing a Null buffer</test_objective>
+    <test_type>Negative</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>1.Ccsp Components  should be in a running state of DUT
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>platform_hal_getFactoryCmVariant</api_or_interface_used>
+    <input_parameters>NULL Buffer</input_parameters>
+    <automation_approch>1.Load halplatform module
+2. Invoke the HAL api platform_hal_getFactoryCmVariant() by passing a null buffer
+3. HAL API is expected to return failure status and the value should not be retrieved because null buffer was passed as an argument
+4. UnLoad halplatform module</automation_approch>
+    <expected_output>platform_hal_getFactoryCmVariant()  should not crash but return failure status on passing NULL buffer as input</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>halplatform</test_stub_interface>
+    <test_script>TS_platform_stub_hal_GetFactoryCmVariant_NullBuffer</test_script>
+    <skipped>No</skipped>
+    <release_version>M82</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("halplatform","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_platform_stub_hal_GetFactoryCmVariant_NullBuffer');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+if "SUCCESS" in loadmodulestatus.upper():
+        obj.setLoadModuleStatus("SUCCESS");
+        #Script to load the configuration file of the component
+        tdkTestObj = obj.createTestStep("platform_stub_hal_getFactoryCmVariant");
+        tdkTestObj.addParameter("flag",1);
+        expectedresult="FAILURE";
+        tdkTestObj.executeTestCase(expectedresult);
+        actualresult = tdkTestObj.getResult();
+        details = tdkTestObj.getResultDetails();
+
+        if expectedresult in actualresult :
+           tdkTestObj.setResultStatus("SUCCESS");
+           print "TEST STEP 1: Retrieve the Platform_FactoryCmVariant with NULL buffer";
+           print "EXPECTED RESULT 1: Should not retrieve the Platform_FactoryCmVariant  with NULL buffer";
+           print "ACTUAL RESULT 1: platform_hal_GetFactoryCmVariant() api call failed";
+           #Get the result of execution
+           print "[TEST EXECUTION RESULT] : SUCCESS";
+        else:
+           #Set the result status of execution
+           tdkTestObj.setResultStatus("FAILURE");
+           print "TEST STEP 1: Retrieve the Platform_FactoryCmVariant with NULL buffer";
+           print "EXPECTED RESULT 1: Should not retrieve the Platform_FactoryCmVariant  with NULL buffer";
+           print "ACTUAL RESULT 1: platform_hal_GetFactoryCmVariant()  api call sucess";
+           #Get the result of execution
+           print "[TEST EXECUTION RESULT] :FAILURE";
+        obj.unloadModule("halplatform");
+else:
+        print "Failed to load the module";
+        obj.setLoadModuleStatus("FAILURE");
+        print "Module loading failed";
