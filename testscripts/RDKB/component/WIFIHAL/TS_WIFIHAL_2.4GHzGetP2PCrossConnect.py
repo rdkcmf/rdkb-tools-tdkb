0333765f46218a0232571d59a2347c8a4f1a6af9
ssurendranpillai <smrithis@tataelxsi.co.in>
2020-11-27 14:47:19 +0000

    TDKB-1533 : M83 HAL test coverage
    Reason for change: Adding test coverage for wifi_hal hotspot2.0 apis
    Test Procedure: Run wifi_hal scripts via TDK TM.
    Risks: None
    Signed-off-by: Smrithi <smrithis@tataelxsi.co.in>
    
    (cherry picked from commit f9edb897917b48147b70d591519a078afa250328)
    Change-Id: Id3d5bc173c6d018e9aac0d4829a48e92d342b52c

---
+++ testscripts/RDKB/component/WIFIHAL/TS_WIFIHAL_2.4GHzGetP2PCrossConnect.py
@@ -0,0 +1,114 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>2</version>
+  <name>TS_WIFIHAL_2.4GHzGetP2PCrossConnect</name>
+  <primitive_test_id/>
+  <primitive_test_name>WIFIHAL_GetOrSetParamBoolValue</primitive_test_name>
+  <primitive_test_version>3</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>Get the current P2PCrossConnect enabled status for 2.4G using wifi_getP2PCrossConnect()</synopsis>
+  <groups_id/>
+  <execution_time>1</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>Broadband</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_WIFIHAL_427</test_case_id>
+    <test_objective>Get the current P2PCrossConnect enabled status for 2.4G using wifi_getP2PCrossConnect()</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>1.Ccsp Components  should be in a running state else invoke cosa_start.sh manually that includes all the ccsp components and TDK Component
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>wifi_getP2PCrossConnect</api_or_interface_used>
+    <input_parameters>index:8</input_parameters>
+    <automation_approch>1. Load wifihal module
+2. Invoke wifi_getP2PCrossConnect() and get the P2PCrossConnect enabled status
+3. Check if status is returned as either Enabled or Disabled
+4.Unload wifihal module</automation_approch>
+    <expected_output>wifi_getP2PCrossConnect() should successfully return the current P2PCrossConnect enabled status</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>wifihal</test_stub_interface>
+    <test_script>TS_WIFIHAL_2.4GHzGetP2PCrossConnect</test_script>
+    <skipped>No</skipped>
+    <release_version>M83</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+  <script_tags/>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("wifihal","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_WIFIHAL_2.4GHzGetP2PCrossConnect');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS");
+
+    #Script to load the configuration file of the component
+    passPoint_2G_Index = 8;
+    tdkTestObj = obj.createTestStep("WIFIHAL_GetOrSetParamBoolValue");
+    tdkTestObj.addParameter("methodName","getP2PCrossConnect");
+    #Radio index stands for SSID index here.
+    tdkTestObj.addParameter("radioIndex", passPoint_2G_Index);
+    expectedresult="SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails();
+    if expectedresult in actualresult and "Enabled" in details or "Disabled" in details:
+	#Set the result status of execution
+	tdkTestObj.setResultStatus("SUCCESS");
+	print "TEST STEP 1: Get the P2PCrossConnect enabled status  for 2.4GHz using wifi_getP2PCrossConnect()";
+        print "EXPECTED RESULT 1: Should get P2PCrossConnect enabled status  for 2.4GHz using wifi_getP2PCrossConnect()";
+	print "ACTUAL RESULT 1: %s" %details;
+	#Get the result of execution
+	print "[TEST EXECUTION RESULT] : SUCCESS";
+    else:
+	#Set the result status of execution
+	tdkTestObj.setResultStatus("FAILURE");
+	print "TEST STEP 1: Get the P2PCrossConnect enabled status  for 2.4GHz using wifi_getP2PCrossConnect()";
+	print "EXPECTED RESULT 1: Should get P2PCrossConnect enabled status  for 2.4GHz using wifi_getP2PCrossConnect()";
+	print "ACTUAL RESULT 1: %s" %details;
+	#Get the result of execution
+	print "[TEST EXECUTION RESULT] : FAILURE";
+    obj.unloadModule("wifihal");
+else:
+        print "Failed to load the module";
+        obj.setLoadModuleStatus("FAILURE");
