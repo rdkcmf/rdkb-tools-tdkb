38fcfa0f5fc9d600e8860e05d353d49024cacf7f
svelus003c <sugasini_velusamy@comcast.com>
2021-01-04 19:10:53 +0000

    RDKB-33519 : Bring TDK as RDM in generic RDKB Image
    Reason for change: Moved the logic of checking the TDK
    RFC parameter value in apps-rdm.sh and download the
    package only if the RFC value is true
    Test Procedure: Check whether TDK RFC parameter is disabled
    by default and TDK is not running.
    Risks: None
    Signed-off-by: svelus003c <sugasini_velusamy@comcast.com>
    
    (cherry picked from commit d6a8714794d091a9b8455f3956165296a1df0000)
    Change-Id: I1eaaf952bc7cb7076ce30750d9d77670ac44ed83

--- agent/scripts/tdkb_launcher.sh
+++ agent/scripts/tdkb_launcher.sh
@@ -19,67 +19,20 @@
 # limitations under the License.
 ##########################################################################
 
-TDK_AGENT_RDM_PATH=/tmp/tdk-b-dl/usr/ccsp/tdk/
+TDK_AGENT_RDM_PATH=/tmp/TDK/usr/ccsp/tdk/
 TDK_AGENT_PATH=/usr/ccsp/tdk/
-TDK_AGENT_LIB_PATH=/tmp/tdk-b-dl/usr/lib/
-DNLD_SCRIPT=/etc/rdm/downloadMgr.sh
-LOG_FILE=/rdklogs/logs/tdkb_launcher.log
-APP_NAME=tdk-b-dl
-VALIDATION_METHOD=openssl
-PKG_EXT=ipk
 
 if [ -f "$TDK_AGENT_PATH/StartTDK.sh" ]; then
-        echo "Found TDK-B Image but not RDM enabled" >> $LOG_FILE
-        echo "Invoking StartTDK.sh from $TDK_AGENT_PATH" >> $LOG_FILE
+        echo "Found TDK-B Image but not RDM enabled"
+        echo "Invoking StartTDK.sh from $TDK_AGENT_PATH"
         sh $TDK_AGENT_PATH/StartTDK.sh
 
 elif [ -f "$TDK_AGENT_RDM_PATH/StartTDK.sh" ]; then
-        echo "Found RDM enabled TDK-B Image" >> $LOG_FILE
-        echo "Invoking StartTDK.sh from $TDK_AGENT_RDM_PATH" >> $LOG_FILE
+        echo "Found RDM enabled TDK-B/RDK-B Image"
+        echo "Invoking StartTDK.sh from $TDK_AGENT_RDM_PATH"
         sh $TDK_AGENT_RDM_PATH/StartTDK.sh
-
 else
-        count=0
-        while [ "$count" -lt "12" ]
-        do
-                echo "Loop Count: $count" >> $LOG_FILE
-                if [ -f /tmp/.xconfssrdownloadurl ]; then
-                        echo "File /tmp/.xconfssrdownloadurl is present...." >> $LOG_FILE
-                        url_status=`cat /tmp/.xconfssrdownloadurl`
-                        if [[ $url_status =~ .*https.* ]]; then
-                                echo "URL found in /tmp/.xconfssrdownloadurl...." >> $LOG_FILE
-                                break
-                        else
-                                echo "URL not found in /tmp/.xconfssrdownloadurl...." >> $LOG_FILE
-                                echo "Waiting for another 60 seconds!!!!!!"
-                                sleep 60
-                        fi
-                else
-                        echo "File /tmp/.xconfssrdownloadurl is not present...." >> $LOG_FILE
-                        sleep 60
-                fi
-                        count=`expr $count + 1`
-        done
-
-        echo "Download the TDK-B package" >> $LOG_FILE
-        sh $DNLD_SCRIPT $APP_NAME "" $VALIDATION_METHOD $PKG_EXT ""
-        DNLD_RES=$?
-        echo "TDK-B package download result is: $DNLD_RES" >> $LOG_FILE
-
-        if [ $DNLD_RES -eq 0 ]; then
-                echo "TDK-B package download is successful" >> $LOG_FILE
-                echo "Sleep for 60 seconds for the package to untar...." >> $LOG_FILE
-                sleep 60
-                export PATH=$PATH:$TDK_AGENT_RDM_PATH
-                export LD_LIBRARY_PATH=/lib:/usr/lib:$TDK_AGENT_LIB_PATH
-                if [ -f "$TDK_AGENT_RDM_PATH/StartTDK.sh" ]; then
-                        echo "Invoking StartTDK.sh from $TDK_AGENT_RDM_PATH" >> $LOG_FILE
-                        sh $TDK_AGENT_RDM_PATH/StartTDK.sh
-                else
-                        echo "TDK-B startup script is not available under $TDK_AGENT_RDM_PATH" >> $LOG_FILE
-                fi
-        else
-                echo "TDK-B Download failed!!! result is: $DNLD_RES" >> $LOG_FILE
-        fi
+	echo "TDK-B startup script is not available under $TDK_AGENT_PATH or $TDK_AGENT_RDM_PATH"
+	echo "TDK-B Binaries are not available!!!! Failed to start TDK!!!!"
 fi
 
