b72f1e9265cecec7d8481a340157869ddc62741a
Raksha <raksha_vaidya@comcast.com>
2021-02-04 06:51:18 +0000

    TDKB-1670 : Syscfg update nvram tr181 parameter is deprecated
    Reason for change: According to RDKB-34001 marking the script as N/A
    Test Procedure: Run scripts via TDK TM
    Risks: None
    Signed-off-by: raksha  <raksha.vaidya@tataelxsi.co.in>
    Change-Id: Ia4b221662d22a37b539bc4ceb52c51251e53370c

--- testscripts/RDKB/component/PAM/TS_PAM_SetSysCfgUpdateNvram.py
+++ testscripts/RDKB/component/PAM/TS_PAM_SetSysCfgUpdateNvram.py
@@ -48,7 +48,7 @@
   <skip>false</skip>
   <!--  -->
   <box_types>
-    <box_type>Broadband</box_type>
+    <box_type></box_type>
     <!--  -->
   </box_types>
   <rdk_versions>
@@ -59,7 +59,7 @@
     <test_case_id>TC_PAM_197</test_case_id>
     <test_objective>To validate the Syscfg.UpdateNvram parameter set using syscfg files availablity </test_objective>
     <test_type>Positive</test_type>
-    <test_setup>Broadband</test_setup>
+    <test_setup></test_setup>
     <pre_requisite>1.Ccsp Components  should be in a running state of DUT that includes component under test Cable Modem
 2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
     <api_or_interface_used>N/A</api_or_interface_used>
