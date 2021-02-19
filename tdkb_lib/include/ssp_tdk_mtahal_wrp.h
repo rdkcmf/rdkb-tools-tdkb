d5eaf867d75e13d7837225b08112bc13302f8101
ssurendranpillai <smrithis@tataelxsi.co.in>
2021-02-19 09:33:27 +0000

    TDKB-1701 : Resolve lib dependencies of TDK
    Reason for change: To remove platform specific lib linking from common Makefile
    Test Procedure: Build tdk enabled image and validate test scripts
    Risks: None
    Signed-off-by: Smrithi <smrithis@tataelxsi.co.in>
    Change-Id: I52082813545ce951c58ed901cfb3805180e79738

--- tdkb_lib/include/ssp_tdk_mtahal_wrp.h
+++ tdkb_lib/include/ssp_tdk_mtahal_wrp.h
@@ -22,24 +22,31 @@
 #define SSP_SUCCESS       0
 #define SSP_FAILURE       1
 
-int ssp_MTAHAL_GetParamCharValue(char* paramName, char* value);
-int ssp_MTAHAL_GetParamUlongValue(char* paramName, unsigned long* value);
-int ssp_MTAHAL_SetParamUlongValue(char* paramName, unsigned long value);
-int ssp_MTAHAL_GetDHCPInfo(PMTAMGMT_MTA_DHCP_INFO pInfo);
-int ssp_MTAHAL_GetLineTableGetEntry(unsigned long index, PMTAMGMT_MTA_LINETABLE_INFO pEntry);
-int ssp_MTAHAL_TriggerDiagnostics(unsigned long index);
-int ssp_MTAHAL_GetServiceFlow(unsigned long* count, PMTAMGMT_MTA_SERVICE_FLOW *ppCfg);
-int ssp_MTAHAL_GetCalls(unsigned long instanceNumber, unsigned long* count, PMTAMGMT_MTA_CALLS* ppCfg);
-int ssp_MTAHAL_GetCALLP(unsigned long lineNumber, PMTAMGMT_MTA_CALLP pCallp);
-int ssp_MTAHAL_GetDSXLogs(unsigned long* count, PMTAMGMT_MTA_DSXLOG* ppDSXLog);
-int ssp_MTAHAL_GetMtaLog(unsigned long* count, PMTAMGMT_MTA_MTALOG_FULL* ppConf);
-int ssp_MTAHAL_getDhcpStatus(MTAMGMT_MTA_STATUS *output_pIpv4status, MTAMGMT_MTA_STATUS *output_pIpv6status);
-int ssp_MTAHAL_getConfigFileStatus(MTAMGMT_MTA_STATUS *poutput_status);
-int ssp_MTAHAL_getLineRegisterStatus(MTAMGMT_MTA_STATUS *output_status_array, int array_size);
-int ssp_MTAHAL_GetHandsets(unsigned long* count, PMTAMGMT_MTA_HANDSETS_INFO* ppHandsets);
-int ssp_MTAHAL_InitDB(void);
-int ssp_MTAHAL_devResetNow(void);
-int ssp_MTAHAL_getMtaOperationalStatus(MTAMGMT_MTA_STATUS *operationalStatus);
-int ssp_MTAHAL_start_provisioning(int mtaIPMode, char* dhcpOption122Suboption1, char* dhcpOption122Suboption2, char* dhcpOption2171CccV6DssID1, char* dhcpOption2171CccV6DssID2);
-int ssp_MTAHAL_LineRegisterStatus_callback_register(void);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    int ssp_MTAHAL_GetParamCharValue(char* paramName, char* value);
+    int ssp_MTAHAL_GetParamUlongValue(char* paramName, unsigned long* value);
+    int ssp_MTAHAL_SetParamUlongValue(char* paramName, unsigned long value);
+    int ssp_MTAHAL_GetDHCPInfo(PMTAMGMT_MTA_DHCP_INFO pInfo);
+    int ssp_MTAHAL_GetLineTableGetEntry(unsigned long index, PMTAMGMT_MTA_LINETABLE_INFO pEntry);
+    int ssp_MTAHAL_TriggerDiagnostics(unsigned long index);
+    int ssp_MTAHAL_GetServiceFlow(unsigned long* count, PMTAMGMT_MTA_SERVICE_FLOW *ppCfg);
+    int ssp_MTAHAL_GetCalls(unsigned long instanceNumber, unsigned long* count, PMTAMGMT_MTA_CALLS* ppCfg);
+    int ssp_MTAHAL_GetCALLP(unsigned long lineNumber, PMTAMGMT_MTA_CALLP pCallp);
+    int ssp_MTAHAL_GetDSXLogs(unsigned long* count, PMTAMGMT_MTA_DSXLOG* ppDSXLog);
+    int ssp_MTAHAL_GetMtaLog(unsigned long* count, PMTAMGMT_MTA_MTALOG_FULL* ppConf);
+    int ssp_MTAHAL_getDhcpStatus(MTAMGMT_MTA_STATUS *output_pIpv4status, MTAMGMT_MTA_STATUS *output_pIpv6status);
+    int ssp_MTAHAL_getConfigFileStatus(MTAMGMT_MTA_STATUS *poutput_status);
+    int ssp_MTAHAL_getLineRegisterStatus(MTAMGMT_MTA_STATUS *output_status_array, int array_size);
+    int ssp_MTAHAL_GetHandsets(unsigned long* count, PMTAMGMT_MTA_HANDSETS_INFO* ppHandsets);
+    int ssp_MTAHAL_InitDB(void);
+    int ssp_MTAHAL_devResetNow(void);
+    int ssp_MTAHAL_getMtaOperationalStatus(MTAMGMT_MTA_STATUS *operationalStatus);
+    int ssp_MTAHAL_start_provisioning(int mtaIPMode, char* dhcpOption122Suboption1, char* dhcpOption122Suboption2, char* dhcpOption2171CccV6DssID1, char* dhcpOption2171CccV6DssID2);
+    int ssp_MTAHAL_LineRegisterStatus_callback_register(void);
+#ifdef __cplusplus
+}
+#endif
 
