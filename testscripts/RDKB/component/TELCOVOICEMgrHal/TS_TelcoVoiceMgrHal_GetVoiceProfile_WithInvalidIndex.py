f2cd62102545580160a88a481594ba90547a630f
Anjali Vadakkepat <Anjali_Vadakkepat@comcast.com>
2021-08-23 11:07:35 +0000

    TDKB-1993 : Telco Voice Manager HAL Coverage Improvement
    Reason for change: Adding Telco Voice Manager HAL scripts
    Test Procedure: Run scripts via TDK TM.
    Risks: None
    Signed-off-by: Anjali V  <Anjali_Vadakkepat@comcast.com>
    Change-Id: I9e16559cfef9909a803b358d2559f5190f41435e

---
+++ testscripts/RDKB/component/TELCOVOICEMgrHal/TS_TelcoVoiceMgrHal_GetVoiceProfile_WithInvalidIndex.py
@@ -0,0 +1,167 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2021 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>2</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TS_TelcoVoiceMgrHal_GetVoiceProfile_WithInvalidIndex</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>TELCOVOICEMgrHal_GetVoiceProfile</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>2</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Check if the Telco Voice Manager HAL Voice Profile structure values are not retrieved when invalid index is passed.</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TELCOVOICEMGRHAL_11</test_case_id>
+    <test_objective>Check if the Telco Voice Manager HAL Voice Profile structure values are not retrieved when invalid index is passed.</test_objective>
+    <test_type>Negative</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>1.Ccsp Components  should be in a running state of DUT
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>json_hal_client_init
+json_hal_client_run
+json_hal_is_client_connected
+json_hal_client_send_and_get_reply 
+json_hal_get_param
+json_hal_add_param</api_or_interface_used>
+    <input_parameters>index : 0
+paramName : Device.Services.VoiceService.1.VoiceProfile.</input_parameters>
+    <automation_approch>1. Load the Telco Voice HAL Module
+2. Invoke the json_hal_client_init() JSON HAL API to initiate the connection to JSON HAL server with TELCO VOICE HAL Schema file
+3. Invoke the json_hal_client_run() JSON HAL API to start the JSON HAL client service.
+4. Invoke the json_hal_is_client_connected() to check whether JSON HAL client is connected to JSON HAL server or not
+5. Return True if json hal client is connected to json hal server else failure
+6. Invoke the json_hal_add_param with the parameter name "0" - invalid voice profile index
+7. Invoke the json_hal_client_send_and_get_reply API to send the request to JSON HAL server
+8. Invoke the json_hal_get_param API to get the value of the structure parameters
+9. Check if the structure values are retrieved with invalid index passed as input.
+10. Unload the Telco Voice HAL Module</automation_approch>
+    <expected_output>Should not retrieve the Telco Voice Manager HAL Voice Profile structure values with invalid index.</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>telcovoicemgrhal</test_stub_interface>
+    <test_script>TS_TelcoVoiceMgrHal_GetVoiceProfile_WithInvalidIndex</test_script>
+    <skipped>No</skipped>
+    <release_version>M92</release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("telcovoicemgrhal","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_TelcoVoiceMgrHal_GetVoiceProfile_WithInvalidIndex');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+
+if "SUCCESS" in loadmodulestatus.upper() :
+    obj.setLoadModuleStatus("SUCCESS");
+
+    tdkTestObj = obj.createTestStep('TELCOVOICEMgrHal_Init');
+    expectedresult = "SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails();
+
+    if expectedresult in actualresult:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "TEST STEP 1: Initiate the TELCOVOICEMgrHal_Init operation";
+        print "EXPECTED RESULT 1: TELCOVOICEMgrHal_Init Should be success";
+        print "ACTUAL RESULT 1: TELCOVOICEMgrHal_Init was success";
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : SUCCESS" ;
+
+        tdkTestObj = obj.createTestStep('TELCOVOICEMgrHal_GetVoiceProfile');
+        invalid_index = 0;
+        tdkTestObj.addParameter("index",invalid_index);
+        tdkTestObj.addParameter("paramName", "Device.Services.VoiceService.1.VoiceProfile.");
+        expectedresult = "FAILURE";
+        tdkTestObj.executeTestCase(expectedresult);
+        actualresult = tdkTestObj.getResult();
+        voice_profile_details = tdkTestObj.getResultDetails();
+
+        if expectedresult in actualresult:
+            #Set the result status of execution
+            tdkTestObj.setResultStatus("SUCCESS");
+            print "TEST STEP 2: Get the Voice Profile details";
+            print "EXPECTED RESULT 2: GetVoiceProfile operation should be failed with invalid index : %d" %invalid_index;
+            print "ACTUAL RESULT 2: GetVoiceProfile operation failed , Details: ",voice_profile_details;
+            #Get the result of execution
+            print "[TEST EXECUTION RESULT] : SUCCESS" ;
+
+        else:
+            #Set the result status of execution
+            tdkTestObj.setResultStatus("FAILURE");
+            print "TEST STEP 2: Get the Voice Profile details";
+            print "EXPECTED RESULT 2: GetVoiceProfile operation should be failed with invalid index : %d" %invalid_index;
+            print "ACTUAL RESULT 2: GetVoiceProfile operation success, Details : ", voice_profile_details;
+            #Get the result of execution
+            print "[TEST EXECUTION RESULT] : FAILURE" ;
+    else:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("FAILURE");
+        print "TEST STEP 1: Initiate the TELCOVOICEMgrHal_Init operation";
+        print "EXPECTED RESULT 1: TELCOVOICEMgrHal_Init Should be Success";
+        print "ACTUAL RESULT 1: TELCOVOICEMgrHal_Init was Failed";
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : FAILURE" ;
+
+    obj.unloadModule("telcovoicemgrhal");
+else:
+    print "Failed to load the module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading failed";
---
+++ testscripts/RDKB/component/TELCOVOICEMgrHal/TS_TelcoVoiceMgrHal_Init.py
@@ -0,0 +1,116 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2021 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>1</version>
+  <name>TS_TelcoVoiceMgrHal_Init</name>
+  <primitive_test_id/>
+  <primitive_test_name>TELCOVOICEMgrHal_Init</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>To invoke the json hal client to connect to json hal server with Telco Voice Manager schema file</synopsis>
+  <groups_id/>
+  <execution_time>2</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>Broadband</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_TELCOVOICEMGRHAL_01</test_case_id>
+    <test_objective>To invoke the json hal client to connect to json hal server with Telco Voice Manager schema file</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>1.Ccsp Components  should be in a running state of DUT
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>json_hal_client_init
+json_hal_client_run
+json_hal_is_client_connected</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1. Load the Telco Voice Manager HAL Module
+2. Invoke the json_hal_client_init() JSON HAL API to initiate the connection to JSON HAL server with TELCO VOICE HAL Schema file
+3. Invoke the json_hal_client_run() JSON HAL API to start the JSON HAL client service.
+4. Invoke the json_hal_is_client_connected() to check whether JSON HAL client is connected to JSON HAL server or not
+5. Return True if json hal client is connected to json hal server else failure
+6. Unload the Telco Voice Manager HAL Module</automation_approch>
+    <expected_output>JSON HAL Client should be connected to JSON HAL server with TELCO VOICE HAL Schema file</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>telcovoicemgrhal</test_stub_interface>
+    <test_script>TS_TelcoVoiceMgrHal_Init</test_script>
+    <skipped>No</skipped>
+    <release_version>M92</release_version>
+    <remarks/>
+  </test_cases>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("telcovoicemgrhal","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_TelcoVoiceMgrHal_Init');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+
+if "SUCCESS" in loadmodulestatus.upper() :
+    obj.setLoadModuleStatus("SUCCESS");
+
+    tdkTestObj = obj.createTestStep('TELCOVOICEMgrHal_Init');
+    expectedresult = "SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails();
+
+    if expectedresult in actualresult:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "TEST STEP 1: Initiate the TELCOVOICEMgrHal_Init operation";
+        print "EXPECTED RESULT 1: TELCOVOICEMgrHal_Init Should be success";
+        print "ACTUAL RESULT 1: TELCOVOICEMgrHal_Init was success";
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : %s" %actualresult ;
+
+    else:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("FAILURE");
+        print "TEST STEP 1: Initiate the TELCOVOICEMgrHal_Init operation";
+        print "EXPECTED RESULT 1: TELCOVOICEMgrHal_Init Should be Success";
+        print "ACTUAL RESULT 1: TELCOVOICEMgrHal_Init was Failed";
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : %s" %actualresult ;
+
+    obj.unloadModule("telcovoicemgrhal");
+else:
+    print "Failed to load the module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading failed";
