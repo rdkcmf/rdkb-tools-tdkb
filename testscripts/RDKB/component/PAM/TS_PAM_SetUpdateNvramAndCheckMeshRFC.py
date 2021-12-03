c179a94e4e53b3de5dcf85a9555b21d70e4300aa
Raksha <raksha_vaidya@comcast.com>
2021-12-03 06:01:18 +0000

    TDKB-1689 : Marking TS_PAM_SetUpdateNvramAndCheckMeshRFC as N/A
    Reason for change: SysCfg.UpdateNvram parameter is not available
    Test Procedure:Run scripts via TDK TM
    Risks: None
    Signed-off-by: raksha vaidya<raksha.vaidya@tataelxsi.co.in>
    Change-Id: I800b5758622af1ab77abd16c207c8d1d1d83dad8

--- testscripts/RDKB/component/PAM/TS_PAM_SetUpdateNvramAndCheckMeshRFC.py
+++ testscripts/RDKB/component/PAM/TS_PAM_SetUpdateNvramAndCheckMeshRFC.py
@@ -33,7 +33,6 @@
   <remarks/>
   <skip>false</skip>
   <box_types>
-    <box_type>Broadband</box_type>
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
@@ -42,7 +41,7 @@
     <test_case_id>TC_PAM_198</test_case_id>
     <test_objective>To Check Mesh Enable.Ovs Enable.Mesh PodEthernetBackhaulEnable parameters are able to set when SysCfg.UpdateNvram was false</test_objective>
     <test_type>Positive</test_type>
-    <test_setup>Broadband</test_setup>
+    <test_setup></test_setup>
     <pre_requisite>1.Ccsp Components  should be in a running state of DUT that includes component under test Cable Modem
 2.TDK Agent should be in running state or invoke it through StartTdk.sh script
 </pre_requisite>
@@ -129,18 +128,18 @@ if "SUCCESS" in loadmodulestatus.upper():
         print "ACTUAL RESULT 1: Get the Initial values are success"
         print "[TEST EXECUTION RESULT] : SUCCESS";
 
-	#Do Factory Reset
-    	obj.saveCurrentState();
-	tdkTestObj = obj.createTestStep('pam_Setparams');
-	tdkTestObj.addParameter("ParamName","Device.X_CISCO_COM_DeviceControl.FactoryReset");
-	tdkTestObj.addParameter("ParamValue","Router,Wifi,VoIP,Dect,MoCA");
-    	tdkTestObj.addParameter("Type","string");
-	expectedresult="SUCCESS";
-	tdkTestObj.executeTestCase(expectedresult);
+        #Do Factory Reset
+        obj.saveCurrentState();
+        tdkTestObj = obj.createTestStep('pam_Setparams');
+        tdkTestObj.addParameter("ParamName","Device.X_CISCO_COM_DeviceControl.FactoryReset");
+        tdkTestObj.addParameter("ParamValue","Router,Wifi,VoIP,Dect,MoCA");
+        tdkTestObj.addParameter("Type","string");
+        expectedresult="SUCCESS";
+        tdkTestObj.executeTestCase(expectedresult);
         obj.restorePreviousStateAfterReboot();
         sleep(180);
 
-	tdkTestObj = obj.createTestStep("pam_GetParameterValues");
+        tdkTestObj = obj.createTestStep("pam_GetParameterValues");
         nvram_result,nvram_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram");
         print "UpdateNvram Param Result %s and Current value %s" %(nvram_result,nvram_details);
 
@@ -160,7 +159,7 @@ if "SUCCESS" in loadmodulestatus.upper():
             print "ACTUAL RESULT 2: Get functions was success after factory reset"
             print "[TEST EXECUTION RESULT] : SUCCESS";
 
-	    if nvram_details == "true" and mesh_details == "false" and ovs_details == "false" and podeth_details == "true":
+            if nvram_details == "true" and mesh_details == "false" and ovs_details == "false" and podeth_details == "true":
                 tdkTestObj.setResultStatus("SUCCESS");
                 print "TEST STEP 3: Verify the Values after Factory Result "
                 print "EXPECTED RESULT 3: Factory Default values should be UpdateNvram as true,Mesh Enable as false, ovs Enable as false and Mesh PodEthernetBackhaulEnable as true"
@@ -168,9 +167,9 @@ if "SUCCESS" in loadmodulestatus.upper():
                 print "[TEST EXECUTION RESULT] : SUCCESS";
                 sleep(10);
 
-		tdkTestObj = obj.createTestStep('pam_SetParameterValues');
-		nvram_set_result,nvram_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram","false");
-		print "UpdateNvram Param set Result %s and Current value %s" %(nvram_set_result,nvram_set_details);
+                tdkTestObj = obj.createTestStep('pam_SetParameterValues');
+                nvram_set_result,nvram_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram","false");
+                print "UpdateNvram Param set Result %s and Current value %s" %(nvram_set_result,nvram_set_details);
 
                 if expectedresult in nvram_set_result:
                     tdkTestObj.setResultStatus("SUCCESS");
@@ -179,12 +178,12 @@ if "SUCCESS" in loadmodulestatus.upper():
                     print "ACTUAL RESULT 4: Set function was success and update nvram value set to false"
                     print "[TEST EXECUTION RESULT] : SUCCESS";
 
-     		    sleep(10);
-		    tdkTestObj = obj.createTestStep("pam_GetParameterValues");
-		    nvram_get_result,nvram_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram");
-		    print "UpdateNvram Param Result %s and Current value %s" %(nvram_get_result,nvram_get_details);
+                    sleep(10);
+                    tdkTestObj = obj.createTestStep("pam_GetParameterValues");
+                    nvram_get_result,nvram_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram");
+                    print "UpdateNvram Param Result %s and Current value %s" %(nvram_get_result,nvram_get_details);
 
-    		    if expectedresult in nvram_get_result:
+                    if expectedresult in nvram_get_result:
                         tdkTestObj.setResultStatus("SUCCESS");
                         print "TEST STEP 5: Verify Update Nvram value as False"
                         print "EXPECTED RESULT 5: Get Function should success and update nvram value should be false"
@@ -192,97 +191,97 @@ if "SUCCESS" in loadmodulestatus.upper():
                         print "[TEST EXECUTION RESULT] : SUCCESS";
 
                         tdkTestObj = obj.createTestStep('pam_SetParameterValues');
-        	        mesh_set_result,mesh_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.Enable",str(not mesh_details).lower());
-		        print "Mesh Enable Param Result %s and Current value %s" %(mesh_set_result,mesh_set_details);
+                        mesh_set_result,mesh_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.Enable",str(not mesh_details).lower());
+                        print "Mesh Enable Param Result %s and Current value %s" %(mesh_set_result,mesh_set_details);
 
-		        ovs_set_result,ovs_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.OVS.Enable",str(not ovs_details).lower());
-		        print "OVS Enable Param Result %s and Current value %s" %(ovs_set_result,ovs_set_details);
+                        ovs_set_result,ovs_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.OVS.Enable",str(not ovs_details).lower());
+                        print "OVS Enable Param Result %s and Current value %s" %(ovs_set_result,ovs_set_details);
 
-		        podeth_set_result,podeth_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.PodEthernetBackhaulEnable",str(not podeth_details).lower());
-		        print "PodEthernetBackhaulEnable Param Result %s and Current value %s" %(podeth_set_result,podeth_set_details);
+                        podeth_set_result,podeth_set_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.PodEthernetBackhaulEnable",str(not podeth_details).lower());
+                        print "PodEthernetBackhaulEnable Param Result %s and Current value %s" %(podeth_set_result,podeth_set_details);
 
-		        if expectedresult in (mesh_set_result and ovs_set_result and podeth_set_result):
+                        if expectedresult in (mesh_set_result and ovs_set_result and podeth_set_result):
                             tdkTestObj.setResultStatus("SUCCESS");
                             print "TEST STEP 6: Set new values to Mesh enable,Mesh PodEthernetBackhaulEnable and Ovs enable when update nvram value was false"
                             print "EXPECTED RESULT 6: Set Function should success"
                             print "ACTUAL RESULT 6: Set function was success"
                             print "[TEST EXECUTION RESULT] : SUCCESS";
 
-		            tdkTestObj = obj.createTestStep("pam_GetParameterValues");
-		            nvram_get_result,nvram_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram");
-		            print "UpdateNvram Param Result %s and Current value %s" %(nvram_get_result,nvram_get_details);
+                            tdkTestObj = obj.createTestStep("pam_GetParameterValues");
+                            nvram_get_result,nvram_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram");
+                            print "UpdateNvram Param Result %s and Current value %s" %(nvram_get_result,nvram_get_details);
 
-        		    mesh_get_result,mesh_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.Enable");
-	        	    print "Mesh Enable Param Result %s and Current value %s" %(mesh_get_result,mesh_get_details);
+                            mesh_get_result,mesh_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.Enable");
+                            print "Mesh Enable Param Result %s and Current value %s" %(mesh_get_result,mesh_get_details);
 
-        		    ovs_get_result,ovs_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.OVS.Enable");
-	        	    print "OVS Enable Param Result %s and Current value %s" %(ovs_get_result,ovs_get_details);
+                            ovs_get_result,ovs_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.OVS.Enable");
+                            print "OVS Enable Param Result %s and Current value %s" %(ovs_get_result,ovs_get_details);
 
-        		    podeth_get_result,podeth_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.PodEthernetBackhaulEnable");
-	        	    print "PodEthernetBackhaulEnable Param Result %s and Current value %s" %(podeth_get_result,podeth_get_details);
+                            podeth_get_result,podeth_get_details = pam_GetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.PodEthernetBackhaulEnable");
+                            print "PodEthernetBackhaulEnable Param Result %s and Current value %s" %(podeth_get_result,podeth_get_details);
 
-            		    if expectedresult in (nvram_get_result and mesh_get_result and ovs_get_result and podeth_get_result):
+                            if expectedresult in (nvram_get_result and mesh_get_result and ovs_get_result and podeth_get_result):
                                 tdkTestObj.setResultStatus("SUCCESS");
                                 print "TEST STEP 7: Get new values of Mesh enable,Mesh PodEthernetBackhaulEnable and Ovs enable"
                                 print "EXPECTED RESULT 7: Get Function should success"
                                 print "ACTUAL RESULT 7: Get function was success"
                                 print "[TEST EXECUTION RESULT] : SUCCESS";
 
-        			if mesh_get_details.upper() == str(not mesh_details).upper()  and ovs_get_details.upper() == str(not ovs_details).upper() and podeth_get_details.upper() == str(not podeth_details).upper():
-	        		    print "Get Values Verified after set";
+                                if mesh_get_details.upper() == str(not mesh_details).upper()  and ovs_get_details.upper() == str(not ovs_details).upper() and podeth_get_details.upper() == str(not podeth_details).upper():
+                                    print "Get Values Verified after set";
                                     tdkTestObj.setResultStatus("SUCCESS");
                                     print "TEST STEP 8: Verify the new values of Mesh enable,Mesh PodEthernetBackhaulEnable and Ovs enable"
                                     print "EXPECTED RESULT 8: The values should be mesh enable as true ,PodEthernetBackhaulEnable as true and ovs enable as false "
                                     print "ACTUAL RESULT 8: Verification was success"
                                     print "[TEST EXECUTION RESULT] : SUCCESS";
 
-        			else:
+                                else:
                                     tdkTestObj.setResultStatus("FAILURE");
                                     print "TEST STEP 8: Verify the new values of Mesh enable,Mesh PodEthernetBackhaulEnable and Ovs enable"
                                     print "EXPECTED RESULT 8: The values should be mesh enable as true ,PodEthernetBackhaulEnable as true and ovs enable as false "
                                     print "ACTUAL RESULT 8: Verification was Failed"
-      		                    print "[TEST EXECUTION RESULT] : FAILURE";
-    		            else:
-			        tdkTestObj.setResultStatus("FAILURE");
+                                    print "[TEST EXECUTION RESULT] : FAILURE";
+                            else:
+                                tdkTestObj.setResultStatus("FAILURE");
                                 print "TEST STEP 7: Get new values of Mesh enable,Mesh PodEthernetBackhaulEnable and Ovs enable"
                                 print "EXPECTED RESULT 7: Get Function should success"
                                 print "ACTUAL RESULT 7: Get function was Failed"
-    		                print "[TEST EXECUTION RESULT] : FAILURE";
+                                print "[TEST EXECUTION RESULT] : FAILURE";
 
-		            #Revert the values
-	    	            tdkTestObj = obj.createTestStep('pam_SetParameterValues');
-		            nvram_revert_result,nvram_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram",nvram_initial_details);
-		            print "UpdateNvram Param set Result %s and Current value %s" %(nvram_revert_result,nvram_revert_details);
-		            sleep(10);
+                            #Revert the values
+                            tdkTestObj = obj.createTestStep('pam_SetParameterValues');
+                            nvram_revert_result,nvram_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SysCfg.UpdateNvram",nvram_initial_details);
+                            print "UpdateNvram Param set Result %s and Current value %s" %(nvram_revert_result,nvram_revert_details);
+                            sleep(10);
 
-		            mesh_revert_result,mesh_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.Enable",mesh_initial_details);
-		            print "Mesh Enable Param Result %s and Current value %s" %(mesh_revert_result,mesh_revert_details);
+                            mesh_revert_result,mesh_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.Enable",mesh_initial_details);
+                            print "Mesh Enable Param Result %s and Current value %s" %(mesh_revert_result,mesh_revert_details);
 
-		            ovs_revert_result,ovs_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.OVS.Enable",ovs_initial_details);
-		            print "OVS Enable Param Result %s and Current value %s" %(ovs_revert_result,ovs_revert_details);
+                            ovs_revert_result,ovs_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.OVS.Enable",ovs_initial_details);
+                            print "OVS Enable Param Result %s and Current value %s" %(ovs_revert_result,ovs_revert_details);
 
-		            podeth_revert_result,podeth_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.PodEthernetBackhaulEnable",podeth_initial_details);
-		            print "PodEthernetBackhaulEnable Param Result %s and Current value %s" %(podeth_revert_result,podeth_revert_details);
+                            podeth_revert_result,podeth_revert_details = pam_SetParameterValues(tdkTestObj,"Device.DeviceInfo.X_RDKCENTRAL-COM_xOpsDeviceMgmt.Mesh.PodEthernetBackhaulEnable",podeth_initial_details);
+                            print "PodEthernetBackhaulEnable Param Result %s and Current value %s" %(podeth_revert_result,podeth_revert_details);
 
-	    	            if expectedresult in (nvram_revert_result and mesh_revert_result and ovs_revert_result and podeth_revert_result):
+                            if expectedresult in (nvram_revert_result and mesh_revert_result and ovs_revert_result and podeth_revert_result):
                                 tdkTestObj.setResultStatus("SUCCESS");
                                 print "TEST STEP 9: Revert the values to initial values"
                                 print "EXPECTED RESULT 9: Revert set operation should success"
                                 print "ACTUAL RESULT 9: Revert operation  was success"
                                 print "[TEST EXECUTION RESULT] : SUCCESS";
-	    	            else:
+                            else:
                                 tdkTestObj.setResultStatus("FAILURE");
                                 print "TEST STEP 9: Revert the values to initial values"
                                 print "EXPECTED RESULT 9: Revert set operation should success"
                                 print "ACTUAL RESULT 9: Revert operation  was Failed"
                                 print "[TEST EXECUTION RESULT] : FAILURE";
-		        else:
+                        else:
                             tdkTestObj.setResultStatus("FAILURE");
                             print "TEST STEP 6: Set new values to Mesh enable,Mesh PodEthernetBackhaulEnable and Ovs enable when update nvram value was false"
                             print "EXPECTED RESULT 6: Set Function should success"
                             print "ACTUAL RESULT 6: Set function was success"
                             print "[TEST EXECUTION RESULT] : FAILURE";
-	            else:
+                    else:
                         tdkTestObj.setResultStatus("FAILURE");
                         print "TEST STEP 5: Verify Update Nvram value as False"
                         print "EXPECTED RESULT 5: Get Function should success and update nvram value should be false"
